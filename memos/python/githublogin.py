f7a9d2828493258f6dd8a69cd29831fe9f317939
Mory <mory2015@163.com>
2016-12-02 00:00:06 +0800

    change my password! :->

--- memos/python/githublogin.py
+++ memos/python/githublogin.py
@@ -1,3 +1,9 @@
+"""
+This script handle the "nonce" when log in github.com 
+special thanks to tha author of <learning scrapy>
+"""
+
+
 from scrapy.spiders import CrawlSpider
 from scrapy.http import Request, FormRequest
 
@@ -16,10 +22,11 @@ class GithubLoginSpider(CrawlSpider):
     def parse_welcome(self, response):
         return FormRequest.from_response(
             response,
-            formdata={"login": "siriusdemon", "password": "2foralfv"}
+            formdata={"login": "yourUsername", "password": "yourPassword"}
         )
 
     def parse(self, response):
+        # of course, you can do more!
         h2 = response.xpath('//*[@id="js-pjax-container"]//h2').extract_first()
         print(h2)
         
