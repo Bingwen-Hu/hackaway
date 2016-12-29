1bbc39d6d21ece4225d80498ee35944a46e7966a
siriusdemon <mory2016@126.com>
2016-12-29 23:00:42 +0800

    scala lisp

---
+++ books/data-science/intro.lisp
@@ -0,0 +1,15 @@
+/**
+  * special thanks to Joel Grus and his book - data-science-from-scratch 
+  * using Scala to rewrite most of codes in his excellect book.
+  */
+
+
+// Linear Algebra
+
+// Vector
+
+def vector_add(v: List[Double], w: List[Double]): List[(Double, Double)] = {
+  v zip w
+}
+
+
