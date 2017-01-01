b8589d8ccc7b664184e4b1d3ea25d3762686bf36
siriusdemon <mory2016@126.com>
2017-01-01 21:27:24 +0800

    do macro

--- books/lisp/onlisp.lisp
+++ books/lisp/onlisp.lisp
@@ -1,5 +1,4 @@
 ;; what's macro?
-
 (defmacro our-expander (name) `(get ,name 'expander))
 
 (defmacro our-defmacro (name parms &body body)
@@ -16,3 +15,29 @@
   (if (and (consp expr) (our-expander (car expr)))
       (funcall (our-expander (car expr)) expr)
       expr))
+
+
+;; do macro
+(defmacro our-do (bindforms (test &rest result) &body body)
+  (let ((label (gensym)))
+    `(prog ,(make-initforms bindforms)
+	,label
+	(if ,test
+	    (return (progn ,@result)))
+	,@body
+	(psetq ,@(make-stepforms bindforms))
+	(go ,label))))
+
+(defun make-initforms (bindforms)
+  (mapcar #'(lambda (b)
+	      (if (consp b)
+		  (list (car b) (cadr b))
+		  (list b nil)))
+	  bindforms))
+			
+(defun make-stepforms (bindforms)
+  (mapcan #'(lambda (b)
+	      (if (and (consp b) (third b))
+		  (list (car b) (third b))
+		  nil))
+	  bindforms))
