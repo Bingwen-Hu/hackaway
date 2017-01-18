2d0c588fee9b358542229c8c7ce8e71590d3050d
siriusdemon <mory2016@126.com>
2017-01-18 15:40:55 +0800

    tree method and draw

--- books/sklearn/supervise-learning.py
+++ books/sklearn/supervise-learning.py
@@ -45,12 +45,108 @@ from scipy.stats import sem
 def evaluate_cross_validation(clf, X, y, K):
     # create a k-fpld cross validation iterator
     cv = KFold(len(y), K, shuffle=True, random_state=0)
-    scores = cross_val_score(clf, X, y, score)
+    scores = cross_val_score(clf, X, y, cv=cv)
     print(scores)
-    print("Mean score: {0:.3f} (+/-{1:.3f})").format(np.mean(scores), 
+    print("Mean score: {0:.3f} (+/-{1:.3f})".format(np.mean(scores), 
                                                      sem(scores)))
 
 
 
 
+# ==================== navie bayes
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# basic one
+clf = Pipeline([
+    ('vect', TfidfVectorizer())
+    ('clf', MultinomialNB())])
+
+# improve step by step
+clf_1 =  Pipeline([
+    ('vect', TfidfVectorizer())
+    ('clf', MultinomialNB(alpha=0.01))])
+
+clf_2 =  Pipeline([
+    ('vect', TfidfVectorizer(token_pattern=r"some regular pattern"))
+    ('clf', MultinomialNB(alpha=0.01))])
+
+
+# can be replace by NLTK
+def get_stop_words():
+    result = set()
+    for line in open('stopwords_en.txt', 'r').readlines():
+        result.add(line.strip())
+    return result
+
+clf_3 =  Pipeline([
+    ('vect', TfidfVectorizer(token_pattern=r"some regular pattern", 
+                             stop_words=get_stop_words()))
+    ('clf', MultinomialNB(alpha=0.01))])
+
+
+
+# ==================== Evaluating the performance
+from sklearn import metrics
+
+def train_and_evaluate(clf, X_train, X_test, y_train, y_test):
+    
+    clf.fit(X_train, y_train)
+    
+    print("Accuracy on training set:")
+    print(clf.score(X_train, y_train))
+    print("Accuracy on testing set:")
+    print(clf.score(X_test, y_test))
+    y_pred = clf.predict(X_test)
+    
+    print("CLassification Report:")
+    print(metrics.classification_report(y_test, y_pred))
+    print("Confusion Matrix:")
+    print(metrics.confusion_matrix(y_test, y_pred))
+
+# Warning: NOT executable plainly
+# ==================== LabelEncoder and OneHotEncoder
+from sklearn,preprocessing import LabelEncoder
+enc = LabelEncoder()
+label_encoder = enc.fit(X_train[:, 3]) # certain column in input data.
+integers_class = label_encoder.transform(label_encoder.classes_) # just for inspect
+
+# change the input data
+t = label_encoder.transform(X_train[:, 3])
+X_train[:, 3] = t
+
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder()
+one_hot_encoder = enc.fit()
+
+# First, convert classes to 0-N-1 integers using label_encoder
+num_of_rows = X_train.shape[0]
+t = label_encoder.transform(X_train[:, 0]).reshape(num_of_rows, 1)
+
+# second, create a sparse matrix with three columns, each one indicating if the instance belongs to the class
+new_features = one_hot_encoder.transform(t)
+
+# add the new_features to the input data
+new_X = np.concatenate([X_train, new_features.toarray()], axis=1)
+
+# delete the origin 
+new_X = np.delete(new_X, [0], 1)
+
+# and update the label and so on......
+
+# ==================== tree method and draw
+# there are decision tree and extra tree
+# the following code using IPython module and Graphviz to draw the tree
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=3, min_samples_leaf=5)
+clf = clf.fit(X_train, y_train)
+
+import pydot.StringIO
+dot_data = StringIO.StringIO()
+tree.export_graphviz(clf, out_file=dot_data, 
+                     feature_name=['age', 'sex', '1st_class', '2nd_class', '3rd_class'])
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('pictureName')
+from IPython.core.display import Image
+Image(filename="pictureName")
