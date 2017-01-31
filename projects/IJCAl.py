4ef1e27c2d5b25f8a8bcfcf02c56c92a8be23e99
siriusdemon <mory2016@126.com>
2017-01-31 20:34:49 +0800

    basic analysis

--- projects/IJCAl.py
+++ projects/IJCAl.py
@@ -35,12 +35,33 @@ extra_user_view = pd.read_csv("extra_user_view.txt", names=user_headers)
 print("shop_info, user_pay, user_view, extra_user_view: ")
 print(shop_info.shape, user_pay.shape, user_view.shape, extra_user_view.shape)
 
+# ==================== what should be excluded and included?
+# shop_id sbould NOT be in training because it is just identifies shops.
+# city_name is an important feature!
+# location_id will confuse the model so should NOT be!
+# per_pay shows the price should be in.
+# numeric: score, comment_cnt, shop_level may be important.
+# cate_1, cate_2, cate_3 are categories messages. Very important.should be ENCODED
+
+# so train features are: 
+# city_name (decode) 122
+# per_pay
+# score
+# comment_cnt
+# shop_level
+# cate_1 (decode)  6 
+# cate_2 (decode)  17
+# cate_3 (decode)  44
+
+
 # shop_info, user_pay, user_view, extra_user_view: 
 # (2000, 10) (100000, 3) (5556715, 3) (4549929, 3)
 
 cities = shop_info['city_name'] # only 122 cities
 locations = shop_info['location_id']
-
+cate_1 = shop_info['cate_1_name']
+cate_2 = shop_info['cate_2_name']
+cate_3 = shop_info['cate_3_name']
 # locations.describe()
 
 # count    2000.000000
@@ -60,3 +81,79 @@ locations = shop_info['location_id']
 # top         上海
 # freq       285
 # Name: city_name, dtype: object
+
+# cate_1
+
+# 0             美食
+# 1          超市便利店
+# 68          休闲娱乐
+# 246         医疗健康
+# 493     美发/美容/美甲
+# 1756          购物
+
+# cate_2
+
+# 0              休闲茶饮
+# 1                超市
+# 4              休闲食品
+# 5              烘焙糕点
+# 6                快餐
+# 10               小吃
+# 11               中餐
+# 17               火锅
+# 19      汤/粥/煲/砂锅/炖菜
+# 22              便利店
+# 23             其他美食
+# 68             网吧网咖
+# 100              烧烤
+# 246              药店
+# 493            美容美发
+# 1756           本地购物
+# 1846           个人护理
+
+# cate_3
+
+# 0          饮品/甜点
+# 1            NaN
+# 2             奶茶
+# 4           生鲜水果
+# 5             面包
+# 6           西式快餐
+# 10          其它小吃
+# 11           东北菜
+# 12          中式快餐
+# 17       麻辣烫/串串香
+# 19             粥
+# 20            蛋糕
+# 23            西餐
+# 28         米粉/米线
+# 43       川味/重庆火锅
+# 44            川菜
+# 60            面点
+# 63           冰激凌
+# 73          其它快餐
+# 76           咖啡厅
+# 81            粤菜
+# 93        其它烘焙糕点
+# 100         中式烧烤
+# 116          江浙菜
+# 151           零食
+# 163     砂锅/煲类/炖菜
+# 165         日韩料理
+# 168          西北菜
+# 183        其它地方菜
+# 184       其它休闲食品
+# 252           海鲜
+# 313           咖啡
+# 325         其它火锅
+# 395       其他餐饮美食
+# 433          湖北菜
+# 458          自助餐
+# 470         美食特产
+# 587        香锅/烤鱼
+# 679          台湾菜
+# 682           闽菜
+# 692           湘菜
+# 762           熟食
+# 818         其它烧烤
+# 1213       上海本帮菜
