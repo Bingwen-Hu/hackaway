d961e1e0feb7732d97b21bd0876e0459f8e08a95
siriusdemon <mory2016@126.com>
2017-02-16 12:23:13 +0800

    common lisp

--- books/lisp/commonlisp.lisp
+++ books/lisp/commonlisp.lisp
@@ -1,5 +1,25 @@
-;; chapter 3
+;;; Paradigms of AI programming 
+;;; case studies in Common Lisp
 
-(defun longer-than (list1 list2)
-  (> (length list1) (length list2)))
+;;; chapter1
+;;; append one list to another list
+(append '(Pat Kim) (list '(John Q Public) 'Sandy))
 
+;;; a recursive function
+
+(defparameter *title*
+  '(Mr Mrs Miss Ms Sir Madam Dr Admiral Major General))
+
+(defun first-name (name)
+  "a function can skip the title and fetch first name"
+  (if (member (first name) *title*)
+      (first-name (rest name))
+      (first name)))
+
+;;; high order function
+
+(defun mappend (fn the-list)
+  "map fn on every item in the-list and append the result"
+  (apply #'append (mapcar fn the-list)))
+
+(defun self-and-double (x) (list x (+ x x)))
