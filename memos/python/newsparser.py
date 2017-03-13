352fda2780adaeea29d15639a4d159f2e5844560
siriusdemon <mory2016@126.com>
2017-03-13 17:18:31 +0800

    newsparser

--- memos/python/newsparser.py
+++ memos/python/newsparser.py
@@ -5,14 +5,47 @@ Created on Tue Mar  7 16:45:41 2017
 @author: Administrator
 """
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from webUtil import character2url, date2timestamp
 
-def main(bt, et, must, either):
-    url = url_constructor(bt, et, must, either)
+defaulttimes = [[2010, 1, 1],
+             [2011, 1, 1],
+             [2012, 1, 1],
+             [2013, 1, 1],
+             [2014, 1, 1],
+             [2015, 1, 1],
+             [2016, 1, 1],
+             [2017, 1, 1]]
+
+
+def df_split(df):
+    company = df['公司'][0]
+    indexfilter = df['新闻'].apply(lambda news: company in news)
+    df = df[indexfilter]
+    indexStrategy = df['新闻'].apply(lambda news: '战略' in news)
+    strategy = df[indexStrategy] 
+    nonStrategy = df[~indexStrategy]
+    return strategy, nonStrategy
+
+def all_df(times, keyword, company):
+    dataframes = pd.DataFrame(columns=['公司', '起始时间', '新闻'])
+    for bt, et in zip(times, times[1:]):
+        df = get_df(bt, et, keyword, company)
+        dataframes = dataframes.append(df, ignore_index=True)
+    return dataframes
+def cleaned_df(first, other):
+    all_others = [o for ox in other for o in ox]
+    first.extend(all_others)
+    return list(set(first))
+
+def get_df(bt, et, keyword, company):
+    url = url_constructor(bt, et, keyword, company)
     soup = preprocess(url)
     first, other = get_all_text(soup)
-    return first, other
+    text = cleaned_df(first, other)
+    df = news_output(text, bt, company)
+    return df
     
 def preprocess(url, params={}):
     news = requests.get(url, params)
@@ -58,15 +91,21 @@ def next_page(soup):
         text = get_text(soup)
         all_text.append(text)
     return all_text
+
+def news_output(text, bt, company):
+    df = pd.DataFrame(columns=['公司', '起始时间', '新闻'])
+    df["新闻"] = text
+    df["公司"] = company
+    df["起始时间"] = str(bt[0])
+    return df
+
+
+if __name__ == '__main__':
+    importants = ['小米', '华为', 
+    '苹果', '微软', '谷歌', 
+    '支付宝', '京东', '亚马逊', # 购物
+    '美团', '百度外卖', # 查看市场份额
+    '滴滴', '摩拜单车']
     
-
-home = "http://news.baidu.com/advanced_news.html"
-
-url = "http://news.baidu.com/ns?from=news&cl=2&bt=1457280000&y0=2016&m0=3&d0=7&y1=2017&m1=3&d1=7&et=1488902399&q1=%22%BA%CF%D7%F7%22&submit=%B0%D9%B6%C8%D2%BB%CF%C2&q3=%A1%B0%C1%AA%CD%A8%A1%B1&q4=&mt=0&lm=&s=2&begin_date=2016-3-7&end_date=2017-3-7&tn=newstitledy&ct=0&rn=50&q6="
-
-url_format = "http://news.baidu.com/ns?from=news&cl=2&bt=1457280000&y0=2016&m0=3&d0=7&y1=2017&m1=3&d1=7&et=1488902399&q1={must}&submit=%B0%D9%B6%C8%D2%BB%CF%C2&q3={either}&q4=&mt=0&lm=&s=2&begin_date=2016-3-7&end_date=2017-3-7&tn=newstitledy&ct=0&rn=50&q6=".format(must=character2url('"合作"'), either=character2url('"联通"'))
-better = "http://news.baidu.com/ns?word=intitle%3A%28%22%E5%90%88%E4%BD%9C%22%20%28%E2%80%9C%E8%81%94%E9%80%9A%E2%80%9D%29%29&pn=100&cl=2&ct=0&tn=newstitledy&rn=50&ie=utf-8&bt=1457280000&et=1488902399"
-
     
-if '__main__' == __name__:
-    pass
+    home = "http://news.baidu.com/advanced_news.html"
