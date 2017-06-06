df7da1e7ea527e1779a4fb494141352086056547
siriusdemon <mory2016@126.com>
2017-06-07 00:03:20 +0800

    cp5

---
+++ books/ProginScala/cp5.scala
@@ -0,0 +1,15 @@
+
+// string
+
+val name = "Mory"
+println(s"Hello $name!")
+
+println(raw"using regex is very easy! \\")
+
+println("""|Scala learn from Python!
+           |let's stand up!""".stripMargin)
+
+// there is no operatior at all!
+
+name indexOf ('r', 1)
+
