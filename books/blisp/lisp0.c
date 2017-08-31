d631b7140de6628949f21f1d47f0ad6bad9e5f6f
siriusdemon <mory2016@126.com>
2017-08-31 16:33:31 +0800

    portable version

--- books/blisp/lisp0.c
+++ books/blisp/lisp0.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+/* if we are compiling on windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+
+static char buffer[2048];
+
+
+/* fake realine function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+
+/* fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the readline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
 
 
 int main(int argc, char** argv) {
   
 
-  /* Print version and Exit Information. */
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
-  /* In a never ending loop */
   while(1) {
 
     /* Output our prompt */
-    fputs("lispy> ", stdout);
+    char* input = readline("lispy> ");
+    add_history(input);
 
-    /* Read a line of user input */
-    fgets(input, 2048, stdin);
-
-
-    /* Echo input back to user. */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+    free(input);
   }
 
   return 0;
