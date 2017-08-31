3f15b84a092e82cee5e0c3492bb97b69e516d78a
siriusdemon <mory2016@126.com>
2017-08-31 16:21:19 +0800

    a lisp prompt

---
+++ books/blisp/lisp0.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+static char input[2048];
+
+
+int main(int argc, char** argv) {
+  
+
+  /* Print version and Exit Information. */
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  /* In a never ending loop */
+  while(1) {
+
+    /* Output our prompt */
+    fputs("lispy> ", stdout);
+
+    /* Read a line of user input */
+    fgets(input, 2048, stdin);
+
+
+    /* Echo input back to user. */
+    printf("No you're a %s", input);
+  }
+
+  return 0;
+}
