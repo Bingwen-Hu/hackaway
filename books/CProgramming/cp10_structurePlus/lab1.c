8bbf50d08cb719de74b296a5780afc9ccca76c6b
siriusdemon <mory2016@126.com>
2017-10-14 09:30:09 +0800

    lab1

---
+++ books/CProgramming/cp10_structurePlus/lab1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+typedef struct employer {
+
+  int number;
+  char *name;
+  
+} employer, *employerPtr;
+
+void main(){
+
+  employerPtr emps;
+
+  int number; 
+  char name[20];
+
+  puts("Enter employee's number and name");
+  scanf("%d %s", &number, name);
+  
+  
+
+
+
+}
