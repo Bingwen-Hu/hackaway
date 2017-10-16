8cca66400d5e08fe87a7f608e4ddc7adb0dae363
siriusdemon <mory2016@126.com>
2017-10-16 16:45:06 +0800

    sequence store list finish

--- books/dataStructure/link-list/seqList.c
+++ books/dataStructure/link-list/seqList.c
@@ -1,48 +1,101 @@
 /* #define WARNING "Mory English" */
+#include <stdio.h>
+
 
 #define MAXSIZE 20             
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
 
-
-
+typedef int Status;
 typedef int ElemType;
-
-typedef struct List {
+typedef struct {
 
   ElemType data[MAXSIZE];
   int length;
 
 } seqList;
 
-int GetElem(seqList L, int i, ElemType *e) {
-  
-  if (L.length == 0 || i < 0 || i > L.length-1)
-    return 0;                   /* something wrong */
-  
-  *e = L.data[i-1];             /* get the value */
-  return 1;                     /* success */
+
+Status InitElem(seqList *L){
+  L->length = 0;
+  return OK;
 }
 
 
-int InsertElem(seqList *L, int i, ElemType e) {
+
+Status GetElem(seqList L, int i, ElemType *e) {
+  
+  if (L.length == 0 || i < 0 || i > L.length-1)
+    return ERROR;               /* something wrong */
+  
+  *e = L.data[i];             /* get the value */
+  return OK;                    /* success */
+}
+
+
+Status InsertElem(seqList *L, int i, ElemType e) {
 
   if ((L->length == MAXSIZE) || (i < 0) || (i > MAXSIZE-1)) 
-    return 0;                  /* something wrong */
+    return ERROR;               /* something wrong */
+
+  while (i > L->length)
+    i--;
+
 
   for(int k = i; k < L->length; k++)
     L->data[k+1] = L->data[k];
   
   L->data[i] = e;               /* insert value */
   L->length++;                  /* increse length */
-  return 1;
+  return OK;
 }
 
-int DeleteElem(seqList *L, int i) {
+Status DeleteElem(seqList *L, int i) {
   if (i < 0 || i >= L->length || L->length == 0)
-    return 0;
+    return ERROR;
   
   for (int k = i; k < L->length; k++)
     L->data[k] = L->data[k+1];
 
   L->length--;
-  return 1;
+  return OK;
 }
+
+Status PrintElem(seqList L){
+  printf("Elements in List: ");
+  for (int i=0; i<L.length; i++)
+    printf("%d\t", L.data[i]);
+  puts("");
+  return OK;
+}
+
+
+void main(){
+
+  seqList list;
+  InitElem(&list);
+
+  InsertElem(&list, 5, 9);
+  InsertElem(&list, 0, 34);
+  InsertElem(&list, 1, 42);
+  InsertElem(&list, 2, 13);
+  PrintElem(list);
+
+  DeleteElem(&list, 0);
+  PrintElem(list);
+
+  ElemType e;
+  GetElem(list, 1, &e);
+  printf("the 1st element is: %d\n", e);
+  
+}
+
+
+/* Test Note:
+
+   Mory version. if insert in arbitray position and cause empty between 
+   data, the data will append to the end of the list
+
+ */
