d587787a07e61ddf5ad11fe3a3e13384e85cb4b3
siriusdemon <mory2016@126.com>
2017-10-18 13:36:54 +0800

    stupid computer

--- books/dataStructure/stack/stack.c
+++ books/dataStructure/stack/stack.c
@@ -1,32 +1,22 @@
+/* using a stack to perform operation  */
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAXSIZE 10
+#define MAXSIZE 100
 
 
-typedef int ElemType;
+typedef char ElemType;
 typedef struct {
-
   ElemType data[MAXSIZE];
   int top;
+}stack;
 
-} stack;
 
-void Init(stack *s, int length){
-  int r;
+void Init(stack *s){
   s->top = 0;
-  
-  if (length > MAXSIZE){
-    puts("Invalid length!");
-    return;
-  }
-
-  for (int i=0; i<length; i++){
-    srand(i);
-    r = rand();
-    s->data[i] = r;
-    s->top++;
-  }
 }
 
 
@@ -42,30 +32,71 @@ void Push(stack *s, ElemType e){
 void Print(stack s){
   printf("stack: ");
   for (int i=0; i<s.top; i++){
-    printf("%-4d", s.data[i]);
+    printf("%-4c", s.data[i]);
   }
   puts("");
 }
 
 
-int Pop(stack *s){
-  int v;
+ElemType Pop(stack *s){
+  ElemType v;
   s->top--;
   v = s->data[s->top];
   return v;
 }
 
 
-void main(){
+int compute(const char string[], int length){
+  int i=0, cres, c1, c2;
+  ElemType e;
+
   stack s;
-  Init(&s, 10);
-
-
-  Print(s);
-
-  int v = Pop(&s);
-  printf("top value is %d\n", v);
-
-  Print(s);
+  Init(&s);
   
+  while(i < length){
+    if ('0' <= string[i] && string[i] <= '9'){
+      Push(&s, string[i]);
+    } else if (string[i] == '-'){
+      c1 = Pop(&s) - 48;
+      c2 = Pop(&s) - 48;
+      cres = c2 - c1;
+      printf("%d - %d = %d\n", c2, c1, cres);
+      e = cres + 48;
+      Push(&s, e);
+    } else if (string[i] == '+'){
+      c1 = Pop(&s) - 48;
+      c2 = Pop(&s) - 48;
+      cres = c2 + c1;
+      printf("%d + %d = %d\n", c2, c1, cres);
+      e = cres + 48;
+      Push(&s, e);
+    } else if (string[i] == '*'){
+      c1 = Pop(&s) - 48;
+      c2 = Pop(&s) - 48;
+      cres = c2 * c1;
+      printf("%d * %d = %d\n", c2, c1, cres);
+      e = cres + 48;
+      Push(&s, e);
+    } else if (string[i] == '/'){
+      c1 = Pop(&s) - 48;
+      c2 = Pop(&s) - 48;
+      cres = c2 / c1;
+      printf("%d / %d = %d\n", c1, c2, cres);
+      e = cres + 48;
+      Push(&s, e);
+    } else {
+      
+    }
+    i++;
+  }
+  return cres;
+}
+
+
+void main(){
+  char string[] = "9 3 1 - 3 * + 9 2 - + ";
+  int len = strlen(string);
+  int res = compute(string, len);
+  
+  printf("the result is %d\n", res);
 }
