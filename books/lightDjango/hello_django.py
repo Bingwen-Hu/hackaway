ba733948344fcfa3889033c5186e3ddb83ef543c
siriusdemon <mory2016@126.com>
2017-11-09 17:15:49 +0800

    shortest server

---
+++ books/lightDjango/hello_django.py
@@ -0,0 +1,34 @@
+import sys
+
+### settings
+from django.conf import settings
+
+settings.configure(
+	DEBUG=True,
+	SECRET_KEY='MoryAndAnn',
+	ROOT_URLCONF=__name__,
+	MIDDLEWARE_CLASSES=(
+		'django.middleware.common.CommonMiddleware',
+		'django.middleware.csrf.CsrfViewMiddleware',
+		'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	),
+)
+
+
+### urls and views
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def index(request):
+	return HttpResponse('Hello World')
+	
+urlpatterns = (
+	url(r'^$', index),
+)
+
+
+if __name__ == '__main__':
+	from django.core.management import execute_from_command_line
+	
+	execute_from_command_line(sys.argv)
