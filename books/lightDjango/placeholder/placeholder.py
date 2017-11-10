16269ca9081d487fc8c9592d1a94004d0cc03258
siriusdemon <mory2016@126.com>
2017-11-10 10:24:29 +0800

    mory light weight project finish!

--- books/lightDjango/placeholder/placeholder.py
+++ books/lightDjango/placeholder/placeholder.py
@@ -1,7 +1,11 @@
 import sys
+import os
+import hashlib
 
 ### settings
 from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
 	DEBUG=True,
@@ -12,6 +16,25 @@ settings.configure(
 		'django.middleware.csrf.CsrfViewMiddleware',
 		'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	),
+	INSTALLED_APPS=(
+		'django.contrib.staticfiles',
+	),
+	
+	STATICFILES_DIRS=(
+		os.path.join(BASE_DIR, 'static'),
+	),
+	STATIC_URL='/static/',
+	
+	TEMPLATES = [
+		{
+			'BACKEND': 'django.template.backends.django.DjangoTemplates',
+			'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+			'APP_DIRS': True,
+			'OPTIONS': {
+				# ... some options here ...
+			},
+		},
+	]
 )
 
 
@@ -19,9 +42,13 @@ settings.configure(
 from django.conf.urls import url
 from django.http import HttpResponse, HttpResponseBadRequest
 from django import forms
-
+from django.core.cache import cache
+from django.views.decorators.http import etag
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
 from io import BytesIO
 from PIL import Image, ImageDraw
+
 
 class ImageForm(forms.Form):
 	"""Form to validate requested placeholder image"""
@@ -33,20 +60,29 @@ class ImageForm(forms.Form):
 		"""Generate an image of the given type and return as row bytes"""
 		height = self.cleaned_data['height']
 		width = self.cleaned_data['width']
-		image = Image.new('RGB', (width, height))
-		draw = ImageDraw.Draw(image)
-		text = "{}x{}".format(width, height)
-		textwidth, textheight = draw.textsize(text)
-		if textwidth < width and textheight < height:
-			texttop = (height - textheight) // 2
-			textleft = (width - textwidth) // 2
-			draw.text((textleft, texttop), text, fill=(255, 255, 255))
-		content = BytesIO()
-		image.save(content, image_format)
-		content.seek(0)
+		# cache
+		key = '{}.{}.{}'.format(width, height, image_format)
+		content = cache.get(key)
+		if content is None:		
+			image = Image.new('RGB', (width, height))
+			draw = ImageDraw.Draw(image)
+			text = "{}x{}".format(width, height)
+			textwidth, textheight = draw.textsize(text)
+			if textwidth < width and textheight < height:
+				texttop = (height - textheight) // 2
+				textleft = (width - textwidth) // 2
+				draw.text((textleft, texttop), text, fill=(255, 255, 255))
+			content = BytesIO()
+			image.save(content, image_format)
+			content.seek(0)
+			cache.set(key, content, 60 * 60)
 		return content
 	
+def generate_etag(request, width, height):
+	content = 'Placeholder: {0}x{1}'.format(width, height)
+	return hashlib.sha1(content.encode('utf-8')).hexdigest()
 	
+@etag(generate_etag)
 def placeholder(request, width, height):
 	form = ImageForm({'height': height, 'width': width})
 	if form.is_valid():
@@ -57,7 +93,11 @@ def placeholder(request, width, height):
 
 
 def index(request):
-	return HttpResponse('Hello World')
+	example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+	context = {
+		'example': request.build_absolute_uri(example)
+	}
+	return render(request, 'home.html', context)
 	
 urlpatterns = (
 	url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name="placeholder"),
