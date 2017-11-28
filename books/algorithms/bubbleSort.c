3f14e56ac21e6991331e016d2876d9f96ba40971
siriusdemon <mory2016@126.com>
2017-11-28 09:58:40 +0800

    code still bad: bubbleSort

--- books/algorithms/bubbleSort.c
+++ books/algorithms/bubbleSort.c
@@ -7,10 +7,25 @@ void println(int A[], int n){
     putchar('\n');
 }
 
+void bubbleSort(int A[], int n){
+    int t;
+    for (int i=0; i<n; i++){
+        for (int j=n; j>i; j--){
+            if (A[j]<A[j-1]){
+                t = A[j];
+                A[j] = A[j-1];
+                A[j-1] = t;
+            }
+        }
+
+    }
+}
+
+
 
 void main(){
-    int A[] = {9, 7, 5, 4, 2, 6, 18, 1, 0, 3};
+    int A[] = {9, 7, 5, 4, 2, 6, 128, 1, 10, 3};
     println(A, 10);
-    bubbleSort2(A, 10);
+    bubbleSort(A, 10);
     println(A, 10);
 }
