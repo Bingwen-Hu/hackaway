7cb139f4b84526a8939b66d99c1a1bd3c3481ce9
siriusdemon <mory2016@126.com>
2017-12-08 18:13:14 +0800

    resnet forward ok, next step--train captcha

--- projects/resnet/Resnet_v2.py
+++ projects/resnet/Resnet_v2.py
@@ -1,10 +1,11 @@
 # without test
 
-
+import time
+import math
 import collections
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-
+from datetime import datetime
 
 class Block(collections.namedtuple('Block', ['scope', 'unit_fn', 'args'])):
     """A named tuple describing a ResNet block.
@@ -171,4 +172,36 @@ def resnet_v2_200(inputs, num_classes=None, global_pool=True, reuse=None, scope=
                      reuse=reuse, scope=scope)
 
 
+# helper function
+def time_tensorflow_run(session, target, info_string):
+    num_steps_burn_in = 10
+    total_duration = 0.0
+    total_duration_squared = 0.0
 
+    for i in range(num_batches + num_steps_burn_in):
+        start_time = time.time()
+        _ = session.run(target)
+        duration = time.time() - start_time
+        if i >= num_steps_burn_in:
+            if not i % 10:
+                print("%s: step %d, duration = %.3f" %
+                      (datetime.now(), i - num_steps_burn_in, duration))
+            total_duration += duration
+            total_duration_squared += duration * duration
+
+    mn = total_duration / num_batches
+    vr = total_duration_squared / num_batches - mn * mn
+    sd = math.sqrt(vr)
+    print('%s: %s across %d steps, %.3f +/- %.3f sec / batch'%
+          (datetime.now(), info_string, num_batches, mn, sd))
+batch_size = 32
+height, width = 40, 100
+inputs = tf.random_uniform((batch_size, height, width, 3))
+with slim.arg_scope(resnet_arg_scope(is_training=False)):
+    net, end_points = resnet_v2_50(inputs, 310)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+num_batches = 100
+time_tensorflow_run(sess, net, "forward")
