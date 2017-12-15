faf1626a279b1b6d3a4cae73ecd46194deac97b2
siriusdemon <mory2016@126.com>
2017-12-15 15:30:04 +0800

    dice

--- memos/lisp/loops.lisp
+++ memos/lisp/loops.lisp
@@ -48,3 +48,31 @@
 ;; loop for conditional
 (loop for x in my-list
      when (evenp x) sum x)
+
+
+;; test
+(defparameter my-sum 0)
+(defparameter dice '(1 1 1 3 6 5))
+(defparameter my-array #(0 0 0 0 0 0))
+
+(dolist (die dice)
+  (incf (aref my-array (1- die)))
+  (when (= die 1)
+    (incf my-sum 100))
+  (when (= die 5)
+    (incf my-sum 50))
+  (when (and (>= (aref my-array (- die 1)) 3)
+	     (and (or (= 2 3 4 6))))
+    (incf my-sum (* 100 die))
+    (decf (aref my-array (1- die)) 3))
+  (when (and (>= (aref my-array (- die 1)) 3)
+	     (= die 1))
+    (incf my-sum (- 1000 300))
+    (decf (aref my-array (1- die)) 3))
+  (when (and (>= (aref my-array (1- die)) 3)
+	     (= die 5))
+    (incf my-sum (- 500 150))
+    (decf (aref my-array (1- die)) 3)))
+  
+    
+     
