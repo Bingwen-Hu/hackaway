349dba76aab1523ddc83b24833e951d1b83a0dfe
siriusdemon <mory2016@126.com>
2018-01-26 16:41:38 +0800

    list processing

--- books/PractialCL/list-process.lisp
+++ books/PractialCL/list-process.lisp
@@ -11,6 +11,51 @@
 ;;; functional programming style
 (append (list 1 2) (list 3 5))
 
-;;; destructive operations
+;;; destructive operations append->nconc
 (nconc (list 1 2 3) (list 2 4))
+; similarly
+; remove -> delete
+; substitute -> nsubstitute
+; reverse -> nreverse
+
+; recycling idiom
+(defun upto (max)
+  (let ((result nil))
+    (dotimes (i max)
+      (push i result))
+    (nreverse result)))
+
+(defparameter *list*)
+(setf *list* (reverse (list 1 2 3 4 5)))
+
+
+;;; list manipulation functions
+(defvar my-list '(2 3 5 6 7)
+  "global lisp")
+(cadr my-list)
+; caar, caadr, ....
+
+(last my-list)
+(butlast my-list)
+(ldiff my-list 2) ; ?
+(make-list 4 :initial-element 8)
+
+; list* is a cross between list and append
+(list* 1 2 3 4 '(mory ann jenny))
+(list 1 2 3 4 'Mory 'Ann 'Jenny)
+(append '(1 2 3 4) '(Mory Ann Jenny))
+
+;revappend: reverse first argument and append to the second
+(revappend '(a b c) '(h i j))
+;revappend -> nreconc
+(consp nil)
+(consp my-list)
+(listp nil)
+(listp my-list)
+
+;;; mapping
+(mapcar #'(lambda (x) (* 2 x)) '(1 2 3))
+(mapcar #'+ (list 1 2 3) (list 20 3 20))
+(maplist #'(lambda (l1 l2) (append l1 l2)) '(1 2 3) '(4 5 6))
+; mapcan mapcon
 
