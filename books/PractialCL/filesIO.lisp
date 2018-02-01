f76cbef53cbaaeea6a4b15dcdbef946e86619a95
siriusdemon <mory2016@126.com>
2018-02-01 12:29:26 +0800

    read S-expression

--- books/PractialCL/filesIO.lisp
+++ books/PractialCL/filesIO.lisp
@@ -3,3 +3,16 @@
 (let ((in (open path)))
   (format t "~a~%" (read-line in))
   (close in))
+
+;; read the whole file
+(let ((in (open path :if-does-not-exist nil)))
+  (when in 
+    (loop for line = (read-line in nil)
+	 while line do (format t "~a~%" line))
+    (close in)))
+
+;; read S-expression
+(defparameter path "E:/Mory/github/hackaway/books/PractialCL/function.lisp")
+(with-open-file (in path)
+  (dotimes (i 7)
+    (eval (read in))))
