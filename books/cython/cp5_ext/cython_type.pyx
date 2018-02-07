973cc40d3489243b1538400cc700aa931767e7df
siriusdemon <mory2016@126.com>
2018-02-07 12:02:14 +0800

    public and private access in Cython

--- books/cython/cp5_ext/cython_type.pyx
+++ books/cython/cp5_ext/cython_type.pyx
@@ -1,6 +1,7 @@
 cdef class Particle_cy:
     """simple Particle extension type."""
-    cdef readonly double mass, position, velocity
+    cdef readonly double mass      # publie access
+    cdef double position, velocity # private access
     
     def __init__(self, m, p, v):
         self.mass = m
