9a6b2848f39764d927a48fbc77ee88bf1032f129
siriusdemon <mory2016@126.com>
2018-02-07 12:04:30 +0800

    public read and write in Cython

--- books/cython/cp5_ext/cython_type.pyx
+++ books/cython/cp5_ext/cython_type.pyx
@@ -1,7 +1,8 @@
 cdef class Particle_cy:
     """simple Particle extension type."""
-    cdef readonly double mass      # publie access
-    cdef double position, velocity # private access
+    cdef readonly double mass      # public access, read only
+    cdef double position           # private access
+    cdef public double velocity    # public access, write and read
     
     def __init__(self, m, p, v):
         self.mass = m
