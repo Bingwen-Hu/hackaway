9ac71093e0bcfce200ba8651c7b1fd92879c6d5d
siriusdemon <mory2016@126.com>
2018-02-07 17:02:47 +0800

    gaussian filter for RGB image in scipy

--- books/CV with Python/cp1/scipy_basic.py
+++ books/CV with Python/cp1/scipy_basic.py
@@ -5,3 +5,10 @@ from scipy.ndimage import filters
 # gaussian filter
 img = np.array(Image.open("E:/Mory/rose.jpg").convert("L"))
 img2 = filters.gaussian_filter(img, 5)
+
+# for RGB img
+img_RGB = np.array(Image.open("E:/Mory/rose.jpg"))
+img2_RGB = np.zeros(img_RGB.shape)
+for i in range(3):
+    img2_RGB[:, :, i] = filters.gaussian_filter(img_RGB[:, :, i], 5)
+img2_RGB = np.uint8(img2_RGB)
