35cdf097aa4247de70b3225192b4b1026710eb2a
siriusdemon <mory2016@126.com>
2018-02-09 09:54:56 +0800

    KMP wait for more insight

--- books/algorithms/stringMatch.c
+++ books/algorithms/stringMatch.c
@@ -15,7 +15,6 @@ void naive_string_matcher(char *T, int length_T, char *P, int length_P){
             printf("Pattern occurs with shift %d\n", i); 
         }
     }
-
 }
 
 // test pass
@@ -30,6 +29,21 @@ void test_naive_string_matcher(){
     naive_string_matcher(T2, strlen(T2), P2, strlen(P2));
 }
 
+// KMP algorithms
+void compute_prefix_function(char *P, int *next, int len){
+}
+
+void test_compute_prefix_function(){
+    char P[] = "adaaadasad";
+    int len = strlen(P);
+    int next[10] = {0};
+    compute_prefix_function(P, next, len);
+    
+    for (int i=0; i<len; i++){
+        printf("%2d ", next[i]); 
+    }
+    puts("");
+}
+
 int main(){
-    test_naive_string_matcher();
 }
