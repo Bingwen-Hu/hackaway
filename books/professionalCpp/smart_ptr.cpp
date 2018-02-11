b9c63394113dad4e2a4306dd3a6d7e94f77cebd7
siriusdemon <mory2016@126.com>
2018-02-11 18:44:21 +0800

    smart pointer

---
+++ books/professionalCpp/smart_ptr.cpp
@@ -0,0 +1,8 @@
+/* smart pointer
+ * unique pointer: one pointer can reference to the data
+ * shared pointer: counting references
+ * weak pointer: reference the shared pointer without incr or desc
+ */
+
+
+
