5edb7eafe7a259bbf2ab760f187c4999f930f4a5
siriusdemon <mory2016@126.com>
2018-02-11 18:27:00 +0800

    autonotes

---
+++ books/professionalCpp/auto.cpp
@@ -0,0 +1,11 @@
+/* four meaning of `auto`
+ * 1. tell the compiler to automatically deduce the type of a variable
+ * 2. for alternative function syntax
+ * 3. function return type deduction
+ * 4. generic lambda expression
+ *
+ * Note: auto and decltype shine in templates.
+ */
+
+
+
