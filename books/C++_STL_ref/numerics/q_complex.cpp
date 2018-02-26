132c3f1159c10d86146fae97380a036306ed2aae
siriusdemon <mory2016@126.com>
2018-02-26 12:27:26 +0800

    cpp cmath and complex

--- books/C++_STL_ref/numerics/q_complex.cpp
+++ books/C++_STL_ref/numerics/q_complex.cpp
@@ -1,3 +1,8 @@
+/* * complex number in C++
+ * 
+ * 
+ * 
+ */
 #include <complex>
 #include <iostream>
 using namespace std;
@@ -6,9 +11,11 @@ int main(){
 
     complex<float> c(1, 2);
     cout << "c=" << c.real() << '+' << c.imag() << 'i' << endl;
-    c.real(3);
-    c.imag(3);
-    c += 1;
+
+    c.real(3);                          // set real part to 3
+    c.imag(3);                          // set imag part to 3
+    c += 1;                             // add 1 to real part
+    c.imag(c.imag()+1);                 // add 1 to imag part
     cout << "new c=" << c.real() << '+' << c.imag() << 'i' << endl;
     cout << "norm (" << c << ") = " << norm(c) << endl;
     cout << "abs  (" << c << ") = " << abs(c)  << endl;
