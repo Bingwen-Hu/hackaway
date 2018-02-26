9daa3626f43bcda5ef3b5eaf7f4dd6a8fd895e68
siriusdemon <mory2016@126.com>
2018-02-26 11:13:03 +0800

    multifile

--- memos/cmake/basic/calculate.c
+++ memos/cmake/basic/calculate.c
@@ -1,28 +1,8 @@
+#include "MathFunction.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * power -Calculate the power of number.
- * @param base: Base value.
- * @param exponent: Exponent value.
- * 
- * @return base raised to the power exponent.
- */ 
-
-double power(double base, int exponent)
-{
-    int result = base;
-
-    if (exponent == 0){
-        return 1;
-    }
-
-    for (int i=1; i < exponent; i++) {
-        result = result * base;
-    }
-
-    return result;
-}
 
 int main(int argc, char *argv[]) {
     
