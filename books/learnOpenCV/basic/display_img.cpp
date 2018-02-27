6e42c063d7b930e6d7787edf2023a0924a00c692
siriusdemon <mory2016@126.com>
2018-02-27 10:36:37 +0800

    opencv video example

--- books/learnOpenCV/basic/display_img.cpp
+++ books/learnOpenCV/basic/display_img.cpp
@@ -4,9 +4,9 @@ int main(int argc, char** argv) {
     cv::Mat img = cv::imread("/home/mory/Downloads/cat.jpeg", -1);
     if ( img.empty() ) return -1;
     
-    cv::namedWindow("cat", cv::WINDOW_AUTOSIZE);
+    // cv::namedWindow("cat", cv::WINDOW_AUTOSIZE);
     cv::imshow("cat", img);
-    
+
     // wait for user to press a key and then exit
     cv::waitKey(0); 
     cv::destroyWindow("cat");
