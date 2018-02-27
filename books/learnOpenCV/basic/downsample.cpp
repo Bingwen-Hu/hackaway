2b885267246f785d08b7e961ac6d5fa63c832a89
siriusdemon <mory2016@126.com>
2018-02-27 11:39:35 +0800

    downsample example

---
+++ books/learnOpenCV/basic/downsample.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+
+int main() {
+    cv::Mat img1, img2;
+
+    cv::namedWindow("Image1", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Image2", cv::WINDOW_AUTOSIZE);
+
+    img1 = cv::imread("/home/mory/Downloads/cat.jpeg");
+    cv::imshow("Image1", img1);
+    cv::pyrDown(img1, img2);
+    cv::imshow("Image2", img2);
+
+    cv::waitKey(0);
+    return 0;
+}
