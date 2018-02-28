5bf5320646d2f403200a610d1db8a3156e964698
siriusdemon <mory2016@126.com>
2018-02-28 12:25:45 +0800

    login test pass

--- books/learnScrapy/bing/bing/spiders/codewars.py
+++ books/learnScrapy/bing/bing/spiders/codewars.py
@@ -1,22 +1,39 @@
 # -*- coding: utf-8 -*-
+""" testing login using scrapy
+1. start_reqeust do the init fetch, get the form data back
+2. parse_login then fill the user and pass field in the form,
+   by doing this, url change to dashboard, nor sign_in
+3. parse the dashboard page
+"""
+
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-from scrapy.http import FormRequest
+from scrapy.http import FormRequest, Request
 
 class CodewarsSpider(CrawlSpider):
     name = 'codewars'
-    allowed_domains = ['codewars']
+    allowed_domains = ['www.codewars.com']
     
     # replace start_urls
     def start_requests(self):
-        formrequest = FormRequest(
+        request = Request(
             "https://www.codewars.com/users/sign_in",
-            formdata={"user[email]": "user@mail",
-                      "user[password]": "password"}
+            callback=self.parse_login
         )
         # wrap in a list
-        return [formrequest]
+        return [request]
+
+    def parse_login(self, response):
+        """using formrequest to copy all other fields
+        from the original response
+        """
+        formrequest = FormRequest.from_response(
+            response, 
+            formdata={"user[email]"    : "username@163.com",
+                      "user[password]" : "password"}
+        )
+        return formrequest
 
     def parse(self, response):
         return {"url": response.url}
