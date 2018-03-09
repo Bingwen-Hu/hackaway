ee513a18f244aacff564e1539d5b6e0e8c888356
siriusdemon <mory2016@126.com>
2018-03-09 10:43:20 +0800

    add comments to ftp

--- memos/python/ftp.py
+++ memos/python/ftp.py
@@ -9,13 +9,12 @@ server = ftplib.FTP(host)
 server.login(username, password)
 
 
-
 def upload(server, filename, bufsize=1024):
     pwd = server.pwd()
     basename = os.path.basename(filename)
     ftp_path = os.path.join(pwd, f"ftp_{basename}")
     with open(filename, 'rb') as file_local:
-        print(ftp_path)
+        # note that "STOR" is a store command!
         server.storbinary("STOR %s" % ftp_path, file_local, bufsize)
 
 upload(server, "/home/mory/hackaway/memos/python/pip.md")
