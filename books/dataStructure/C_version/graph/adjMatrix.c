68ec26a6f36d10aa5914de3347202262c7646093
siriusdemon <mory2016@126.com>
2018-03-15 14:54:29 +0800

    adjmatrix.c

---
+++ books/dataStructure/C_version/graph/adjMatrix.c
@@ -0,0 +1,57 @@
+/** Adjacency Matrix represent graph
+ */
+#include <stdio.h>
+
+typedef char VertexType;
+typedef int EdgeType;
+#define MAXVEX 100
+#define INFINITY 1024
+
+typedef struct {
+    VertexType vexs[MAXVEX];        // point table
+    EdgeType arc[MAXVEX][MAXVEX];   // relation table
+    int numVertexes, numEdges;
+} MGraph;
+
+void CreateMGraph(MGraph *G){
+    int i, j, k, w;
+    printf("Input number of points and edges: (num_p,num_e)\n");
+    scanf("%d,%d", &G->numVertexes, &G->numEdges);
+    printf("Points: %d\nEdges: %d\n", G->numVertexes, G->numEdges);
+
+    printf("start enter %d chars: \n", G->numVertexes);
+    for (i = 0; i < G->numVertexes; i++)
+        G->vexs[i] = getchar();
+    getchar();
+    for (i = 0; i < G->numVertexes; i++)
+        for (j = 0; j < G->numVertexes; j++)
+            G->arc[i][j] = INFINITY;
+    
+    for (k = 0; k < G->numEdges; k++){
+        printf("input subscrpit i,j and weight w: (i,j,w)\n"); 
+        scanf("%d,%d,%d", &i, &j, &w);
+        printf("arc[%d][%d]=%d\n", i, j, w);
+        G->arc[i][j] = w;
+        G->arc[j][i] = G->arc[i][j];
+    }
+}
+
+
+void printGraph(MGraph *G){
+    printf("Graph information\n"); 
+    printf("-----------------\n");
+    for (int i=0; i < G->numVertexes; i++){
+        for (int j=0; j < G->numVertexes; j++){
+            printf("%6d", G->arc[i][j]);     
+        }
+        puts("\n");
+    }
+}
+
+int main(){
+    MGraph m = {.arc = {0}};
+    m.numVertexes = 5;
+    m.numEdges = 5;
+    CreateMGraph(&m);
+    printGraph(&m);
+}
