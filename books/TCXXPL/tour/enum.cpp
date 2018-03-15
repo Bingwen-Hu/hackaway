2f2fbff772f162633453a66567761d3b09441f23
siriusdemon <mory2016@126.com>
2018-03-15 11:56:16 +0800

    enum class

---
+++ books/TCXXPL/tour/enum.cpp
@@ -0,0 +1,23 @@
+/* enumeration in cpp */
+#include <iostream>
+
+
+enum class Color {red, blue, green};
+enum class Traffic_light {green, yellow, red};
+
+Traffic_light& operator++(Traffic_light& t)
+{
+    switch (t) {
+        case Traffic_light::green: 
+            return t = Traffic_light::yellow;
+        case Traffic_light::yellow:
+            return t = Traffic_light::red;
+        case Traffic_light::red:
+            return t = Traffic_light::green;
+    }    
+}
+
+int main(){
+    Traffic_light light = Traffic_light::red;
+    ++light;
+}
