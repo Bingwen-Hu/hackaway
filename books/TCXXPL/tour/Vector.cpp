6d5906f6a3d7398bbf7a200a9d7a5850fb7a8e9b
siriusdemon <mory2016@126.com>
2018-03-15 11:44:57 +0800

    Vector class

---
+++ books/TCXXPL/tour/Vector.cpp
@@ -0,0 +1,34 @@
+/* I am a Vector class */
+
+#include <iostream>
+
+using namespace std;
+
+class Vector {
+public:
+    Vector(int s): elem{new double[s]}, sz{s} {}
+    double& operator[](int i) {return elem[i];}
+    int size() {return sz;}
+private:
+    double *elem;
+    int sz;
+};
+
+double read_and_sum(int s)
+{
+    Vector v(s);
+    for (int i=0; i != v.size(); i++){
+        cin >> v[i];
+    }
+
+    double sum = 0;
+    for (int i=0; i != v.size(); i++){
+        sum += v[i];
+    }
+    return sum;
+}
+
+int main(){
+    double res = read_and_sum(4);
+    cout << "sum is: " << res << endl;
+}
