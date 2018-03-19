b4cf9ea02ba227ebdb470af3e931a2dcc93778a5
siriusdemon <mory2016@126.com>
2018-03-19 10:56:00 +0800

    Prim test pass

--- books/dataStructure/C_version/graph/adjMatrix.c
+++ books/dataStructure/C_version/graph/adjMatrix.c
@@ -120,10 +120,50 @@ void BFSTraverse(MGraph G){
 }
 
 
+// Mini Span Tree algorithms -- Prim
+void MiniSpanTree_Prim(MGraph *G){
+    int min;
+    int adjvex[MAXVEX];
+    int lowcost[MAXVEX];
+    
+    // set v0 as 0, if lowcost[i] == 0 then it means
+    // i is in span tree
+    lowcost[0] = 0;
+    adjvex[0] = 0;                          
+    // initalization
+    for (int i=1; i < G->numVertexes; i++){ // except v0
+        lowcost[i] = G->arc[0][i];
+        adjvex[i] = 0;                      // init as 0
+    }
+
+    for (int i=1; i < G->numVertexes; i++){
+        min = INFINITY;
+        int j = 1, k = 0;
+        while (j < G->numVertexes){
+            if (lowcost[j] != 0 && lowcost[j] < min){
+                min = lowcost[j];
+                k = j;
+            }
+            j++;
+        }
+
+        printf("(%d, %d)", adjvex[k], k);   // print current lowest weight
+        lowcost[k] = 0;                     // accept the point
+
+        for (j = 1; j < G->numVertexes; j++){
+            if (lowcost[j] != 0 && G->arc[k][j] < lowcost[j]){
+                lowcost[j] = G->arc[k][j];
+                adjvex[j] = k;
+            }
+        }
+    }
+}
+
 int main(){
-    MGraph m = {.arc = {0}};
+    MGraph m;
     CreateMGraph(&m);
     printGraph(&m);
     DFSTraverse(m);
     BFSTraverse(m);
+    MiniSpanTree_Prim(&m);
 }
