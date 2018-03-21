ab779fb1cebcac241c81401c06ba50ee7cdf6363
siriusdemon <mory2016@126.com>
2018-03-21 12:41:15 +0800

    add beautiful docs to first

--- books/dataStructure/Rust_version/lists/src/first.rs
+++ books/dataStructure/Rust_version/lists/src/first.rs
@@ -1,6 +1,30 @@
 /* define recursive data structure using Box */
-pub enum List {
+pub struct List {
+    head: Link,
+}
+
+enum Link {
     Empty,
-    ElemThenEmpty(i32),
-    ElemThenNotEmpty(i32, Box<List>),
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+
+/** 3 primary forms that self can take
+ * ---------------------------------------------------------
+ * Type      | meanings           | behaviours             |
+ * ---------------------------------------------------------
+ * self      | value              | will move              |
+ * &mut self | mutable reference  | like reference in C++  |
+ * &self     | shared reference   | like const reference   |
+ * ---------------------------------------------------------
+ */
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
 }
