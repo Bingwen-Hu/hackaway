8fa12dde22d60650040013d3bc39565da5e4df9f
siriusdemon <mory2016@126.com>
2018-03-21 10:10:00 +0800

    function types

--- books/TRPL/quicklearn/compounds.rs
+++ books/TRPL/quicklearn/compounds.rs
@@ -1,7 +1,7 @@
 /* when I say compound I means list, tuple, array and so and so on */
 
-fn show_tuple(){
-    println!("show tuple!");
+fn show_tuple(message: &'static str){
+    println!("{}", message);
     let tuple: (i32, f64, char) = (42, 3.14, 'A');
     // Warning: Rust love snake case name?
     let (secret, pi, a) = tuple;
@@ -11,14 +11,14 @@ fn show_tuple(){
 }
 
 /* array in Rust is similar to C */
-fn show_array(){
-    println!("Show array!");
+fn show_array(message: &'static str){
+    println!("{}", message);
     let a = [1, 2, 4, 5, 7, 8];
     
     println!("First value of a is {}", a[0]);
 }
 
 fn main(){
-    show_tuple();
-    show_array();
+    show_tuple("show tuple!");
+    show_array("show array!");
 }
