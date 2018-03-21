987546fe4fb694236db24d738761820b60b19001
siriusdemon <mory2016@126.com>
2018-03-21 15:26:52 +0800

    lambda expression

--- books/dataStructure/Rust_version/lists/src/second.rs
+++ books/dataStructure/Rust_version/lists/src/second.rs
@@ -32,15 +32,11 @@ impl List {
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match self.head.take() {
-            None => None,
-            // move value from boxed_node first
-            Some(node) => {
-                let node = *node;
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+        self.head.take().map(|node| {
+            let node = *node;
+            self.head = node.next;
+            node.elem
+        })
     }
 }
 
