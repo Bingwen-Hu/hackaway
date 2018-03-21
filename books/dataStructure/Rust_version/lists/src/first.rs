f2b481240c3104d25d237e172eb9f02052d8c7d3
siriusdemon <mory2016@126.com>
2018-03-21 14:26:28 +0800

    link list test pass

--- books/dataStructure/Rust_version/lists/src/first.rs
+++ books/dataStructure/Rust_version/lists/src/first.rs
@@ -55,3 +55,30 @@ impl List {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::List;
+    
+    #[test]
+    fn basic() {
+        let mut list = List::new();
+        
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(4);
+
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(5);
+        list.push(6);
+        
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+
+    }
+}
