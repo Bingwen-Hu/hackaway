8f5846b20ae146935db9e85c732a4aabb1109dbd
siriusdemon <mory2016@126.com>
2018-04-03 10:06:13 +0800

    return loops

---
+++ books/RBE/cp8_flowcontrol/returnloops.rs
@@ -0,0 +1,16 @@
+// experimental
+// loop as expression and return a value
+
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+}
