79810a4f47d16f40d5d7289bb185aedeb1534fa7
siriusdemon <mory2016@126.com>
2018-04-09 17:43:06 +0800

    Match with value

--- books/TRPL/cp6_enum_and_pattern_matching/match.rs
+++ books/TRPL/cp6_enum_and_pattern_matching/match.rs
@@ -1,8 +1,19 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    So,
+    And,
+    Soon,
+    Etc,
+    Haha,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState), // bind value here
 }
 
 
@@ -14,12 +25,19 @@ fn value_in_cents(coin: Coin) -> i32 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 fn main() {
     let coin = Coin::Penny;
+    let v = value_in_cents(coin);
+    println!("{}", v);
+
+    let coin  = Coin::Quarter(UsState::Haha);
     let v = value_in_cents(coin);
     println!("{}", v);
 
