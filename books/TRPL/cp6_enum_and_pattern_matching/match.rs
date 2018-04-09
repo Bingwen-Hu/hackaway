ff53d7561ddf7be1be9f3cfbfb38c02cceb6f29f
siriusdemon <mory2016@126.com>
2018-04-09 17:38:07 +0800

    basic match in Rust

---
+++ books/TRPL/cp6_enum_and_pattern_matching/match.rs
@@ -0,0 +1,26 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+
+fn value_in_cents(coin: Coin) -> i32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let coin = Coin::Penny;
+    let v = value_in_cents(coin);
+    println!("{}", v);
+
+}
