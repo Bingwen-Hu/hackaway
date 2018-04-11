0a844a04d63cff5f914c837c12fbbe0156db8b17
siriusdemon <mory2016@126.com>
2018-04-11 16:35:45 +0800

    Rule of module in Rust

--- books/TRPL/cp7_module/communicator/src/network/mod.rs
+++ books/TRPL/cp7_module/communicator/src/network/mod.rs
@@ -1,3 +1,7 @@
 fn connect() {}
 // inside module
 mod server;
+
+// Rule of module
+// If a module named foo has no submodules, you should put the declarations for foo in a file named foo.rs.
+// If a module named foo does have submodules, you should put the declarations for foo in a file named foo/mod.rs.
