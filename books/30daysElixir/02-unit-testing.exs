cbdb54eb8bd659023e40e8e5bb3b6fa183d7a8a0
siriusdemon <mory2016@126.com>
2018-04-11 17:11:29 +0800

    elixir testting

--- books/30daysElixir/02-unit-testing.exs
+++ books/30daysElixir/02-unit-testing.exs
@@ -2,9 +2,13 @@ ExUnit.start
 
 defmodule MyTest do
   use ExUnit.Case
-  
+
   test 'simple test' do
     assert 1 + 1 == 2
-
   end
+
+  test "refute is opposite of assert" do
+    refute 1 + 1 == 3
+  end
+
 end
