b25d15f72dd50b0258bb92f435b94aebc785f400
siriusdemon <mory2016@126.com>
2018-04-12 16:38:17 +0800

    Rust get value from vector

--- books/TRPL/cp8_collections/vectors.rs
+++ books/TRPL/cp8_collections/vectors.rs
@@ -11,10 +11,22 @@ fn vpush() {
     v.push(5);
     v.push(1);
     v.push(6);
-    print!("{:?}", v);
+    println!("{:?}", v);
+}
+
+// like get in Python
+fn vget() {
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(v) => println!("{}", v),
+        _ => (),
+    }
 }
 
 fn main() {
     construct();
     vpush();
+    vget();
 }
