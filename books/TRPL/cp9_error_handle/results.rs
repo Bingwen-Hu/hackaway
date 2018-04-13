701091dade21cbbeeb0902208ec12c539e5ac92e
siriusdemon <mory2016@126.com>
2018-04-13 11:17:33 +0800

    handle multi error

--- books/TRPL/cp9_error_handle/results.rs
+++ books/TRPL/cp9_error_handle/results.rs
@@ -1,8 +1,9 @@
 // recoverable error
 
 use std::fs::File;
+use std::io::ErrorKind;
 
-fn main() {
+fn basic() {
     let f = File::open("panics.rs");
 
     let f = match f {
@@ -11,4 +12,28 @@ fn main() {
             panic!("There was a problem opening the file: {:?}", error);
         },
     };
+}
+
+fn multi_error() {
+    let f = File::open("multi_error.rs");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("multi_error.rs") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!("Tried to create file but fail: {:?}", e);
+                },
+            }
+        },
+        Err(error) => {
+            panic!("There was a problem opening the file {:?}", error);
+        },
+    };
+}
+
+fn main() {
+    basic();
+    multi_error();
 }
