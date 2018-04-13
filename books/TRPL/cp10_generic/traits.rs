005336cd51e607b0da255f8fb153758f9a765d7b
siriusdemon <mory2016@126.com>
2018-04-13 15:06:43 +0800

    simple trait example

---
+++ books/TRPL/cp10_generic/traits.rs
@@ -0,0 +1,42 @@
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("Mory"),
+        content: String::from("Mory want to be helpful"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summary());
+}
