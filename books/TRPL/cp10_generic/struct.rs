923281a7a5855c1894b33389e5a087151fb6cd4c
siriusdemon <mory2016@126.com>
2018-04-13 12:47:20 +0800

    generic struct

---
+++ books/TRPL/cp10_generic/struct.rs
@@ -0,0 +1,26 @@
+// generic struct
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = Point {x: 5, y: 10.4};
+    let p2 = Point {x: "hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    // println!("p1.x = {}, p1.y = {}", p1.x, p1.y); move
+    // println!("p2.x = {}, p2.y = {}", p2.x, p2.y); move
+}
