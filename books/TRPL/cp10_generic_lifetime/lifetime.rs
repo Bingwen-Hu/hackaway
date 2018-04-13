caad93c82e9d6058b1722a6de869d0b66b661e0e
siriusdemon <mory2016@126.com>
2018-04-13 16:07:08 +0800

    lifetime a taste!

--- books/TRPL/cp10_generic_lifetime/lifetime.rs
+++ books/TRPL/cp10_generic_lifetime/lifetime.rs
@@ -4,6 +4,8 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("the longest string is {}", result);
+
+    struct_with_lifetime();
 }
 
 // this could not compile because the lifttime of return value 
@@ -15,4 +17,19 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     } else {
         y
     }
+}
+
+// struct hold reference needs a lifetime annotation
+#[derive(Debug)]
+struct ImportExcerpt<'a> {
+    part: &'a str,
+}
+
+fn struct_with_lifetime() {
+    let novel = String::from("Call me Ishmael.");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a `.`");
+    let i = ImportExcerpt { part: first_sentence };
+    println!("{:?}", i);
 }
