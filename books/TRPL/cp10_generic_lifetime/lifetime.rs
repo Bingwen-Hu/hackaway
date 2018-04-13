687ed71e4651c4e0dcadf25588d072a7bb992a1a
siriusdemon <mory2016@126.com>
2018-04-13 15:47:16 +0800

    lifetime in Rust

---
+++ books/TRPL/cp10_generic_lifetime/lifetime.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("the longest string is {}", result);
+}
+
+// this could not compile because the lifttime of return value 
+// is unknown
+// define a generic lifetime annotations and solve the problem
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
