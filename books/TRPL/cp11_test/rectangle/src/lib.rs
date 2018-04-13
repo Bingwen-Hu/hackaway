64e324c9ba11081c18c8c16fbbc47c75afe636a9
siriusdemon <mory2016@126.com>
2018-04-13 17:53:41 +0800

    should_panic with expected string pass

--- books/TRPL/cp11_test/rectangle/src/lib.rs
+++ books/TRPL/cp11_test/rectangle/src/lib.rs
@@ -73,7 +73,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(211);
     }
