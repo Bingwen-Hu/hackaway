a003e73d3947859014550e2b8e20654a36d0fac3
siriusdemon <mory2016@126.com>
2018-04-16 15:10:53 +0800

    successful reading file

--- books/TRPL/cp12_greprs_project/greprs/src/main.rs
+++ books/TRPL/cp12_greprs_project/greprs/src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 
 fn main() {
     let args : Vec<String> = env::args().collect();
@@ -6,6 +9,13 @@ fn main() {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for `{}`", query);
+    println!("In file `{}`", filename);
+
+    let mut f = File::open(filename).expect("File not Found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file.");
+
+    println!("with text: \n{}", contents);
 }
