7b4d3746a15ecb677e23d6a11d8a24cf4e3a152e
siriusdemon <mory2016@126.com>
2018-04-16 17:11:20 +0800

    using stderr instead of stdout

--- books/TRPL/cp12_greprs_project/greprs/src/main.rs
+++ books/TRPL/cp12_greprs_project/greprs/src/main.rs
@@ -7,17 +7,20 @@ use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+    let mut stderr = std::io::stderr();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        writeln!(
+            &mut stderr,
+            "Problem parsing arguments: {}", 
+            err
+        ).expect("Could not write to stderr");
         process::exit(1);
     });
 
-    println!("Searching for `{}`", config.query);
-    println!("In file `{}`", config.filename);
-
     if let Err(e) = greprs::run(config) {
-        println!("Application error: {}", e);
+        writeln!(&mut stderr, "Application error: {}", e)
+            .expect("could not write to stderr");
         process::exit(1);
     }
 }
