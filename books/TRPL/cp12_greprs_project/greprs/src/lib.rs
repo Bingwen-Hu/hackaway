64754620055f444e7f82f694ffe7e45ca78ea935
siriusdemon <mory2016@126.com>
2018-04-16 16:44:38 +0800

    Yah! first version!

--- books/TRPL/cp12_greprs_project/greprs/src/lib.rs
+++ books/TRPL/cp12_greprs_project/greprs/src/lib.rs
@@ -32,7 +32,9 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("with text: \n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
