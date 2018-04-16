e796a191218056b2b4f5d6be6325c93f4a41c4e2
siriusdemon <mory2016@126.com>
2018-04-16 18:05:21 +0800

    iterator in RUst

---
+++ books/TRPL/cp13_functional/iterators.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn intro() {
+    let v1 = vec![1, 2, 3];
+    let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, [2, 3, 4]);
+    println!("{:?}", v1);
+}
+
+fn main() {
+    intro();
+
+    let sum: u32 = Counter::new().take(5)
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    println!("{}", sum);
+}
