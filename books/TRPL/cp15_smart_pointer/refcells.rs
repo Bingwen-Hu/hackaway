84bb2eaf90343c8b01c15edd6ad6ce1f417319dc
siriusdemon <mory2016@126.com>
2018-04-17 15:25:25 +0800

    refcell

---
+++ books/TRPL/cp15_smart_pointer/refcells.rs
@@ -0,0 +1,29 @@
+// refcell allow interior mutability
+// interior mutability means mutating data even though there are 
+// immutable references to that data.
+
+
+// something strange will happen!
+
+use std::cell::RefCell;
+
+fn a_fn_that_immutably_borrows(a: &i32) {
+    println!("a is {}", a);
+}
+
+fn a_fn_that_mutably_borrows(b: &mut i32) {
+    *b += 1;
+}
+
+fn demo(r: &RefCell<i32>) {
+    a_fn_that_immutably_borrows(&r.borrow());
+    a_fn_that_mutably_borrows(&mut r.borrow_mut());
+    a_fn_that_immutably_borrows(&r.borrow());
+}
+
+fn main() {
+    // even data is immutable
+    let data = RefCell::new(5);
+    // value is change
+    demo(&data);
+}
