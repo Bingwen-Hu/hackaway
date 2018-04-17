49bc71142929f2e18356bb7581b16e9c15b0a530
siriusdemon <mory2016@126.com>
2018-04-17 17:24:26 +0800

    thread basic

---
+++ books/TRPL/cp16_fearless_concurrent/threads.rs
@@ -0,0 +1,21 @@
+/* Rust only implement 1:1 thread */
+
+use std::thread;
+use std::time::Duration;
+
+
+fn main() {
+    let joinhandle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    // wait for subthread to finish
+    joinhandle.join();
+}
