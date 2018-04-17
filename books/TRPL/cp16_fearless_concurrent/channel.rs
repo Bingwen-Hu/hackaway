e8a26db7c7535aa6b39d24c31795ffe1c6f57863
siriusdemon <mory2016@126.com>
2018-04-17 18:01:52 +0800

    channel demo

---
+++ books/TRPL/cp16_fearless_concurrent/channel.rs
@@ -0,0 +1,47 @@
+// a channel comtains two parts:
+// a transmitter and a receiver
+
+
+// mspc stands for 
+// multiple producer, single consumer
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn basic() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+    /* see also try_recv */
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+fn more_sender() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("Hi"),
+            String::from("From"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn main() {
+    more_sender();   
+}
