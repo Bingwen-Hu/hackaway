7a7175bdefa207c22b9f65c254d76aef9e3ce255
siriusdemon <mory2016@126.com>
2018-04-17 18:29:42 +0800

    mutex in Rust

--- books/TRPL/cp16_fearless_concurrent/mutex.rs
+++ books/TRPL/cp16_fearless_concurrent/mutex.rs
@@ -1,8 +1,11 @@
 /* mutual exclusive */
+use std::thread;
+// Arc means atomic rc, concurrent safely
+use std::sync::{Mutex, Arc};
 
-use std::sync::Mutex;
 
-fn main() {
+fn basic() {
+    // mutex is a smart pointer
     let m = Mutex::new(5);
 
     {
@@ -12,4 +15,29 @@ fn main() {
     }
 
     println!("m = {:?}", m);
+}
+
+fn shared() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("result: {}", *counter.lock().unwrap());
+}
+
+fn main() {
+    shared();
 }
