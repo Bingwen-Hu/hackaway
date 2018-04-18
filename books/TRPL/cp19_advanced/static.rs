e134a69747216caf8123e0538cc04aedd096fb06
siriusdemon <mory2016@126.com>
2018-04-18 16:54:36 +0800

    global variable

---
+++ books/TRPL/cp19_advanced/static.rs
@@ -0,0 +1,17 @@
+// global variable in Rust
+
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+fn main() {
+    add_to_count(3);
+
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
+}
