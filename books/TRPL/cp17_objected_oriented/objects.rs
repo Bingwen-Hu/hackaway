bb93cd88738de0aca6de03b531f7891591146893
siriusdemon <mory2016@126.com>
2018-04-18 11:22:51 +0800

    object in Rust

---
+++ books/TRPL/cp17_objected_oriented/objects.rs
@@ -0,0 +1,52 @@
+pub struct AverageCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AverageCollection {
+    fn new() -> Self {
+        AverageCollection{
+            list: vec![],
+            average: 0.,
+        }
+    }
+
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            },
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
+
+
+fn main() {
+    let mut ac = AverageCollection::new();
+    ac.add(12);
+    ac.add(42);
+    println!("Average of ac is {}", ac.average());
+
+    ac.add(9);
+    println!("Average of ac is {}", ac.average());
+
+    ac.remove();
+    println!("Average of ac is {}", ac.average());
+}
