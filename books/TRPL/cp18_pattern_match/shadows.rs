edaa30de6fc7a292f663f17307a6905c8d446686
siriusdemon <mory2016@126.com>
2018-04-18 15:28:12 +0800

    binding and shadows

---
+++ books/TRPL/cp18_pattern_match/shadows.rs
@@ -0,0 +1,30 @@
+fn main() {
+    not_shadow();
+}
+
+fn shadow() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
+
+// using match guard
+fn not_shadow() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {:?}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
