44a8c13e2a309354dbcd2f5f987b82b8d9cf07af
siriusdemon <mory2016@126.com>
2018-04-18 15:28:02 +0800

    match will not get ownership

--- books/TRPL/cp18_pattern_match/multi.rs
+++ books/TRPL/cp18_pattern_match/multi.rs
@@ -1,13 +1,13 @@
 fn main() {
     let x = 1;
 
-    // copy trait here
+    
     match x {
         1 | 2 => println!("one or two"),
         _ => println!("others"),
     }
 
-    // copy trait here
+    
     let x = 'b';
     match x {
         'A' ... 'Z' => println!("A through Z"),
