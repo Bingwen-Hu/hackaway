5b45ecd9d7cb30076c22f04210dd1b40f989e980
siriusdemon <mory2016@126.com>
2018-05-03 16:43:48 +0800

    data structure restart

---
+++ books/dataStructureUsingC/README.md
@@ -0,0 +1,3 @@
+# Data structure using C
+## slow my heart
+### I know every thing cause a result.
---
+++ books/dataStructureUsingC/cp1_introduction/first.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("\n welcome to the world of C ");
+    return 0;
+}
