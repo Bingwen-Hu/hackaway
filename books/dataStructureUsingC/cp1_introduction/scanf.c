e5d9bdb8998ebf3a5ff4f74c63ab5d7fcc79bd37
siriusdemon <mory2016@126.com>
2018-05-03 17:22:38 +0800

    scanf over

--- books/dataStructureUsingC/cp1_introduction/scanf.c
+++ books/dataStructureUsingC/cp1_introduction/scanf.c
@@ -12,7 +12,7 @@ void skip() {
     int age;
     char name[20];
     puts("input name as (age*name)");
-    scanf("%d*%s", &age, name);
+    scanf(" %d*%s", &age, name);
     printf("My name is %s, I am %d years old\n", name, age);
 }
 
