6da89436f85793560f3e20a36fecb2d3fc2db5f1
siriusdemon <mory2016@126.com>
2018-05-10 14:29:30 +0800

    check parenthese

--- books/dataStructureUsingC/cp6_stack/applications.c
+++ books/dataStructureUsingC/cp6_stack/applications.c
@@ -27,19 +27,39 @@ void reverse_list() {
     puts("");
 }
 
-void parenthese_checker() {
-    char *valid = "{ A + B * (C - D)}";
-    char *invalid = "(A + B} * (C + I)";
+int parenthese_checker(char *str) {
     
     stack s = make();
-    while (*valid != '\0') {
-        
+    while (*str != '\0') {
+        if (*str == '(' || *str == '{') {
+            push(&s, *str);
+        }
+        if (*str == ')') {
+            if ('(' != pop(&s)) {
+                return -1;
+            }
+        }
+        if (*str == '}') {
+            if ('{' != pop(&s)) {
+                return -1;
+            }
+        }
+        str++;
     }
+    return 1;
 }
 
 
 int main(int argc, char const *argv[])
 {
     reverse_list();
+
+    char *valid = "{ A + B * (C - D)}";
+    char *invalid = "(A + B} * (C + I)";
+    int good = parenthese_checker(valid);
+    printf("%s is %s\n", valid, good > 0 ? "valid" : "invalid");
+    int bad = parenthese_checker(invalid);
+    printf("%s is %s\n", invalid, bad > 0 ? "valid" : "invalid");
+
     return 0;
 }
