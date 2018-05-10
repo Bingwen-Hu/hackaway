b61a976ff1cf8fdcf03bad7e9b42187eb9592915
siriusdemon <mory2016@126.com>
2018-05-10 11:34:57 +0800

    lstack finish

--- books/dataStructureUsingC/cp6_stack/lstack_test.c
+++ books/dataStructureUsingC/cp6_stack/lstack_test.c
@@ -9,6 +9,8 @@ int main() {
     s = push(s, 2);
     s = push(s, 42);
     
+    printf("test peek: %s\n", peek(s) == 42 ? "ok" : "not ok");
+
     int out;
     s = pop(s, &out);
     printf("test pop: %s\n", out == 42 ? "ok" : "not ok");
