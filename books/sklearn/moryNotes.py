422056bf22d09dcd70741c099693ce91c54c252b
siriusdemon <mory2016@126.com>
2018-05-28 09:38:04 +0800

    uncontrol code

--- books/sklearn/moryNotes.py
+++
@@ -1,8 +0,0 @@
-# ==================== LDA and QDA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
-
-# scalable
-
-
-
