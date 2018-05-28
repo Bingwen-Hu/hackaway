af2aece6f831eb37a7bb239d16a342038c5e56c1
siriusdemon <mory2016@126.com>
2018-05-28 09:40:28 +0800

    remove uncontrol code

--- memos/clojure/keystroke/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to keystroke
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- memos/clojure/keystroke/src/keystroke/core.clj
+++
@@ -1,9 +0,0 @@
-(ns keystroke.core
-  (:import [jline.console ConsoleReader]))
-
-(defn show-keystroke []
-  (print "Enter a keystroke: ")
-  (flush)
-  (let [cr (ConsoleReader.)
-        keyint (.readCharacter cr)]
-    (println (format "Got %d ('%c')!" keyint (char keyint)))))
