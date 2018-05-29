fd9f97cf0c731e67b168388adac149a252c21742
siriusdemon <mory2016@126.com>
2018-05-29 09:38:47 +0800

    tree start

---
+++ books/dataStructureUsingC/cp8_tree/tree.h
@@ -0,0 +1,28 @@
+/** some basic
+ * 
+ * ------------- basic concept ---------------
+ * Root: topmost node in a tree.
+ * Subtree: treenodes under one tree node T is considered as T's subtrees
+ * Leaf: A node has no children called leaf node.
+ * Ancestor: parent's parent's ...
+ * Descendant: children's children's ...
+ * Level number: children is Parent + 1
+ * Degree: Degree of a node is equal to the number of children that a node has.
+ * * -----------------------------------------
+ * 
+ * ------------- six tree types --------------
+ * General trees
+ * Forests
+ * Binary trees
+ * Binary search trees
+ * Expression trees
+ * Tournament trees
+ * -------------------------------------------
+ */
+
+
+
+// binary search tree
+// all left node is less than root node
+// all right node is larger than root node.
+
