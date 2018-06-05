1375f5653f88d7774e83150692fbabd7c591fef5
siriusdemon <mory2016@126.com>
2018-06-05 20:39:45 +0800

    symbol and variable in Common lisp

--- books/lisp4AI/cp1_introduce/symbol.lisp
+++ books/lisp4AI/cp1_introduce/symbol.lisp
@@ -1,6 +1,11 @@
+;;; symbol in Common Lisp is case-insensitive
+;;; special symbol in other language is useable in Lisp
+;;; such as $ -> and so on.
+
 (print 'John)
 (print '(John Q Public))
 (print '(+ 2 2))
 (print (+ 2 2))
 
 (append '(Pat Kim) (list '(John Q Public) 'Sandy))
+(length '(Pat Kim (list '(Haha J Mory) 'Ann)))
---
+++ books/lisp4AI/cp1_introduce/variable.lisp
@@ -0,0 +1,8 @@
+;;; variable
+(defparameter x 10)
+(defparameter p 10)
+(setf p '(John Q Public))
+(print p)
+(setf x 10)
+(print (+ x x))
+(print (+ x (length p)))
