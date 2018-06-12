0168ce322235612b54167634bbfc74a4ff5148a3
siriusdemon <mory2016@126.com>
2018-06-12 12:36:40 +0800

    worldcup up and run

--- projects/flask/worldcup.py
+++ projects/flask/worldcup.py
@@ -13,7 +13,7 @@ import json
 app = Flask(__name__)
 
 SERVER_PARAMS = {
-    'host': "119.84.122.135",
+    'host': "0.0.0.0",
     'port': 27702,
     'user': 'like_jian',
     'password': 'worldcup2018',
@@ -31,14 +31,27 @@ def predict(gpid):
         sqls = "select * from Predict where GPid=%s" % gpid
         cursor.execute(sqls)
         result = cursor.fetchone()
-    
+        sqldict = predict_parser(result)
+    jsondata = json.dumps(sqldict)
+    return jsondata
 
 
-def predict_parser(gpid, args=None):
-    pass
+def predict_parser(result):
+    return {
+        'code': 0,
+        'GPid': result[0],
+        'host': result[1],
+        'guest': result[2],
+        'points': result[3] if result[3] else result[8],
+        'rate': result[4] if result[4] else result[7],
+        'bloger': result[5] if result[5] else result[9],
+        'portrait': result[6] if result[6] else "",
+    }
 
 
 
 
 if __name__ == '__main__':
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(host='localhost', port=38016, debug=True)
