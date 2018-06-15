d84f9816b039310905842fb4af9235347416de4e
siriusdemon <mory2016@126.com>
2018-06-15 17:02:07 +0800

    a straight forward

--- books/lisp4AI/cp2_sentence_generative/sentence-generative.lisp
+++ books/lisp4AI/cp2_sentence_generative/sentence-generative.lisp
@@ -1,5 +1,4 @@
 (defun sentence () (append (noun-phrase) (verb-phrase)))
-(defun noun-phrase () (append (Article) (Noun)))
 (defun verb-phrase () (append (Verb) (noun-phrase)))
 (defun Article () (one-of '(the a)))
 (defun Noun () (one-of '(man ball woman table)))
@@ -12,8 +11,29 @@
   "Choose an element from a list at random"
   (elt choices (random (length choices))))
 
-;;; test it
-(print (sentence))
 
 ;;; trace them
-(trace sentence noun-phrase verb-phrase article noun verb)
+; (trace sentence noun-phrase verb-phrase article noun verb)
+(defun Adj* ()
+  (if (= (random 2) 0)
+      nil 
+      (append (Adj) (Adj*))))
+
+(defun PP* ()
+  (if (random-elt '(t nil))
+      (append (PP) (PP*))
+      nil))
+
+(defun noun-phrase () 
+  (append (Article) (Adj*) (Noun) (PP))
+
+(defun Adj ()
+  (one-of '(big little blue green adiabatic)))
+
+(defun PP ()
+  (append (Prep) (noun-phrase)))
+
+(defun Prep ()
+  (one-of '(to in by with on)))
+
+
