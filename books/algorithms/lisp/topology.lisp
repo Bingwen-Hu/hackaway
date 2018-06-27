61000ced81d632171966b79b1c0a19365956b7a6
siriusdemon <mory2016@126.com>
2018-06-27 17:06:03 +0800

    topology sort finish

--- books/algorithms/lisp/topology.lisp
+++ books/algorithms/lisp/topology.lisp
@@ -16,11 +16,57 @@
                   (G D)))
 
 (defun in-degree (graph node)
-  (length 
-   (remove-if-not #'(lambda (lst) 
-                      (member node (rest lst)))
-                  graph)))
+  (count-if #'(lambda (lst) 
+		(member node (rest lst)))
+	    graph))
+
+(defun headnodes (graph)
+  (mapcar #'first graph))
+
+(defun narrow-graph (graph nodes)
+  "remove nodes from graph, not really"
+  (remove-if #'(lambda (lst)
+		 (member (first lst) nodes))
+	     graph))
+
+;; zero-in-degree
+(defun zero-in-degree (graph)
+  "return nodes that is zero in degree"
+  (let ((nodes (headnodes graph)))
+    (remove-if-not #'(lambda (node)
+		       (when (= 0 (in-degree graph node))
+			 node))
+		   nodes)))
+    
+
+(defun terminal-node (graph)
+  (let ((terminal 
+	 (remove-if-not #'(lambda (lst)
+			    (eql nil (rest lst)))
+			graph)))
+    (first (first terminal))))
+
 
 
 (defun topology-sort (graph)
-  )
+  (let ((queue nil)
+	(terminal (terminal-node graph)))
+    (labels ((helper (q)
+	       (setf q (append q (zero-in-degree (narrow-graph graph q))))
+	       (if (member terminal q)
+		   q
+		   (helper q))))
+      (helper queue))))
+	
+
+;; another terminal: zero-in-degree return nil
+
+(defun topology-sort-2 (graph)
+  (let ((queue nil))
+    (labels ((helper (q)
+	       (let ((zero-nodes (zero-in-degree (narrow-graph graph q))))
+		 (if zero-nodes
+		     (helper (append q zero-nodes))
+		     q))))
+      (helper queue))))
+
