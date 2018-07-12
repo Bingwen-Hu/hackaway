6f9cd9fc239d4e5ff56fd2b4d30a0eadab7d3159
siriusdemon <mory2016@126.com>
2018-07-12 11:39:06 +0800

    dynamic route

--- projects/flask/mysite.py
+++ projects/flask/mysite.py
@@ -5,6 +5,7 @@ from flask import request
 
 app = Flask(__name__)
 
+# level 0
 @app.route('/')
 def home_page():
     html = """
@@ -17,6 +18,13 @@ def home_page():
     """
     return html
 
+# level 1
+@app.route('/<name>')
+def sayhello(name):
+    html = f"""
+    <h3>hello, {name}</h3>
+    """
+    return html
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000, debug=True)
