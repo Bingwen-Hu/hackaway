2342b176b6218ea2db854208c912eb47e75bd88a
siriusdemon <mory2016@126.com>
2018-08-27 15:01:47 +0800

    correct parse function

--- projects/pytorch-yolov3-tiny/darknet.py
+++ projects/pytorch-yolov3-tiny/darknet.py
@@ -13,6 +13,7 @@ def parse_cfg(cfgfile):
     in a form of list of dicts """
     file = open(cfgfile, 'r')
     lines = file.read().split('\n')
+    file.close()
     lines = [x for x in lines if len(x) > 0]
     lines = [x for x in lines if x[0] != "#"]
     lines = [x.strip() for x in lines]
@@ -29,7 +30,8 @@ def parse_cfg(cfgfile):
         else:
             key, value = line.split('=')
             block[key.strip()] = value.strip()
-        blocks.append(block)
+    blocks.append(block)
+    
     return blocks
 
 
@@ -38,5 +40,95 @@ def create_modules(blocks):
     module_list = nn.ModuleList()
     prev_filters = 3
     output_filters = []
-    
+
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential()
+        if x['type'] == 'convolutional':
+            activation = x['activation']
+            try:
+                batch_normalize = int(x['batch_normalize'])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+            filters = int(x['filters'])
+            padding = int(x['pad'])
+            kernel_size = int(x['size'])
+            stride = int(x['stride'])
+
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+            
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+            module.add_module('conv_{}'.format(index), conv)
+
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module('batch_norm_{}'.format(index), bn)
+            
+            if activation == 'leaky':
+                activn = nn.LeakyReLU(0.1, inplace=True)
+                module.add_module('leaky_{}'.format(index), activn)
+
+        elif x['type'] == 'upsample':
+            stride = int(x['stride'])
+            upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+            module.add_module('upsample_{}'.format(index), upsample)
+
+        elif x['type'] == 'route':
+            x['layers'] = x['layers'].split(',')
+            try:
+                start, end = [int(ind) for ind in x['layers']]
+            except:
+                start, end = int(x['layers'][0]), 0
+            
+            if start > 0:
+                start = start - index
+            if end > 0:
+                end = end - index
+            
+            route = EmptyLayer()
+            module.add_module('route_{}'.format(index), route)
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
+            else: # not end
+                filters = output_filters[index + start]
+        
+        elif x['type'] == 'shortcut':
+            shortcut = EmptyLayer()
+            module.add_module('shortcut_{}'.format(index), shortcut)
+        
+        elif x['type'] == 'yolo':
+            mask = x['mask'].split(',')
+            mask = [int(x) for x in mask]
+            anchors = x['anchors'].split(',')
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module('Detection_{}'.format(index), detection)
+        
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+        
     return net_info, module_list
+
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+    
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+
+blocks = parse_cfg('/home/mory/hackaway/projects/pytorch-yolov3-tiny/cfg/yolov3.cfg')
+print(create_modules(blocks))
