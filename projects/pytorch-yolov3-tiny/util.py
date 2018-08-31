1cf954fd0530c174842dd2e8587f9fa54ccce372
siriusdemon <mory2016@126.com>
2018-08-31 16:27:31 +0800

    util.py refine

--- projects/pytorch-yolov3-tiny/util.py
+++ projects/pytorch-yolov3-tiny/util.py
@@ -7,12 +7,11 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 from PIL import Image
-
+import cv2
 
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters())
-
 
 def convert2cpu(matrix):
     if matrix.is_cuda:
@@ -65,10 +64,12 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
     prediction[:, :, :4] *= stride
     return prediction
 
+
 def load_classes(namesfile):
     with open(namesfile, "r") as fp:
         names = fp.read().strip().split("\n")
     return names
+
 
 def unique(tensor):
     tensor_np = tensor.cpu().numpy()
@@ -78,6 +79,7 @@ def unique(tensor):
     tensor_res = tensor.new(unique_tensor.shape)
     tensor_res.copy_(unique_tensor)
     return tensor_res
+
 
 def write_results(prediction, confidence, num_classes, nms = True, nms_conf = 0.4):
     conf_mask = (prediction[:,:,4] > confidence).float().unsqueeze(2)
@@ -192,3 +194,27 @@ def bbox_iou(box1, box2):
     iou = inter_area / (b1_area + b2_area - inter_area)
     
     return iou
+
+
+def letterbox_image(img, inp_dim):
+    '''resize image with unchanged aspect ratio using padding'''
+    img_w, img_h = img.shape[1], img.shape[0]
+    w, h = inp_dim
+    new_w = int(img_w * min(w/img_w, h/img_h))
+    new_h = int(img_h * min(w/img_w, h/img_h))
+    resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)
+    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+    canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w,  :] = resized_image
+    return canvas
+
+
+        
+def prep_image(img, inp_dim):
+    """ Prepare image for inputting to the neural network. 
+    Returns a Variable  """
+    orig_im = cv2.imread(img)
+    dim = orig_im.shape[1], orig_im.shape[0]
+    img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
+    img_ = img[:,:,::-1].transpose((2,0,1)).copy()
+    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+    return img_, orig_im, dim
