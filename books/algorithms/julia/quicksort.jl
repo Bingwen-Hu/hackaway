5eb0e92764fb8c99f6516cf6c4fa4849e4ddc9ae
siriusdemon <mory2016@126.com>
2018-09-17 11:27:48 +0800

    random quicksort

--- books/algorithms/julia/quicksort.jl
+++ books/algorithms/julia/quicksort.jl
@@ -11,7 +11,11 @@ function swap(lst, i, j)
 end
 
 
-function partition(lst, p, r)
+function partition(lst, p, r, random=false)
+    if random == true
+        r_ = randint(p, r+1)
+        swap(lst, r, r_)
+    end
     x = lst[r]
     i = p
     for j in p:r-1
@@ -26,10 +30,12 @@ end
 
 lst = [8, 11, 2, 1, 4, 19, 9, 14, 13, 5, 6, 22, ]
 len = length(lst)
-function quicksort(lst, p, r)
+function quicksort(lst, p, r, random=false)
     if p < r
-        q = partition(lst, p, r)
+        q = partition(lst, p, r, random)
+        println(q)
         quicksort(lst, p, q-1)
         quicksort(lst, q+1, r)
     end
 end
+
