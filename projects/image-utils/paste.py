3835dc4b27dfd9bff42e312ef2d5a740d466b2ae
siriusdemon <mory2016@126.com>
2018-09-21 16:03:57 +0800

    paste update

--- projects/image-utils/paste.py
+++ projects/image-utils/paste.py
@@ -62,17 +62,61 @@ def sogou_paste(piecepaths, savepath=None):
 
     return image
 
+def sogou_paste2(piecepaths, savepath=None):
+    image = Image.new('RGB', (140, 44), color=(170, 170, 170))
+    codes = [os.path.basename(path)[:clen] for (clen, path) in piecepaths]
+    pieces = [Image.open(path) for (clen, path) in piecepaths]
+
+    x = 0
+    for p in pieces:
+        w, _ = p.size
+        image.paste(p, [x, 0, x+w, 44])
+        x = x + w
+
+    if savepath is not None:
+        code = ''.join(codes)
+        savepath = os.path.join(savepath, f"{code}{uuid1()}.jpg")
+        image.save(savepath)
+
+    return image
+
 
 if __name__ == '__main__':
     import glob 
     import random
     
-    heads = glob.glob('E:/captcha-data/images/new500/crops/heads/*.png')
-    tails = glob.glob('E:/captcha-data/images/new500/crops/tails/*.png')
+    w1_c1 = glob.glob('E:/captcha-data/images/new81/sogou200/1/1/*.png')
     
+    w2_c1 = glob.glob('E:/captcha-data/images/new81/sogou200/2/1/*.png')
+    w3_c4 = glob.glob('E:/captcha-data/images/new81/sogou200/3/4/*.png')
+    
+    w2_c3 = glob.glob('E:/captcha-data/images/new81/sogou200/2/3/*.png')
+    w3_c2 = glob.glob('E:/captcha-data/images/new81/sogou200/3/2/*.png')
+
+    w2_c2 = glob.glob('E:/captcha-data/images/new81/sogou200/2/2/*.png')
+    w3_c3 = glob.glob('E:/captcha-data/images/new81/sogou200/3/3/*.png')
+    
+    combines = [
+        [w1_c1, w2_c1, w3_c4],
+        [w1_c1, w2_c2, w3_c3],
+        [w1_c1, w2_c3, w3_c2],
+    ]
+    modes = [
+        [1, 1, 4],
+        [1, 2, 3], 
+        [1, 3, 2],
+    ]
+
     for i in range(100000):
-        pieces = random_select_pieces(heads, 5)
-        tail = random.choice(tails)
-        pieces.append(tail)
-        random.shuffle(pieces)
-        image = sogou_paste(pieces, "E:/captcha-data/sogou/rgen4/")
+        r = np.random.randint(0, 10)
+        if r <= 1:
+            r = 0
+        elif 1 < r <= 8:
+            r = 1
+        elif 8 < r <= 10:
+            r = 2
+        combine = combines[r]
+        mode = modes[r]
+        dictpaths = [(m, random.choice(lst)) for (m, lst) in zip(mode, combine)]
+        random.shuffle(dictpaths)
+        image = sogou_paste2(dictpaths, "E:/captcha-data/sogou/rgen5/")
