a63bbcd75162a1e3b11b2d770ada3625a61239bc
siriusdemon <mory2016@126.com>
2018-09-21 12:33:30 +0800

    first function is understand ;-)

--- projects/densenet_pytorch/model.py
+++ projects/densenet_pytorch/model.py
@@ -15,7 +15,6 @@ def _bn_function_factory(norm, relu, conv):
         concated_features = torch.cat(inputs, 1)
         bottleneck_output = conv(relu(norm(concated_features)))
         return bottleneck_output
-
     return bn_function
 
 
