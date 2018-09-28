85c8109fd7a03cb3b669e8be46359701f5152639
siriusdemon <mory2016@126.com>
2018-09-28 18:06:14 +0800

    slim change

--- projects/densenet_pytorch/train.py
+++ projects/densenet_pytorch/train.py
@@ -5,23 +5,6 @@ from config import config
 from preprocess import train_data_iterator, test_data_helper
 
 
-class nCrossEntropyLoss(torch.nn.Module):
-
-    def __init__(self, n=6):
-        super(nCrossEntropyLoss, self).__init__()
-        self.n = n
-        self.total_loss = 0
-        self.loss = nn.CrossEntropyLoss()
-    
-    def forward(self, output, label):
-        output_t = output[:, 0:26]
-        label = torch.autograd.Variable(torch.LongTensor(label.data.cpu().numpy())).cuda()
-        label_t = label[:,0]   
-        for i in range(1, self.n):
-            output_t = torch.cat((output_t, output[:,26*i:26*i+26]), 0)
-            label_t = torch.cat((label_t, label[:,i]), 0)         
-            self.total_loss = self.loss(output_t, label_t)
-        return self.total_loss
 
 net = DenseNet(
     growth_rate=32,
@@ -38,7 +21,7 @@ optimizer = torch.optim.SGD(net.parameters(), lr=0.1, momentum=0.9, nesterov=Tru
 
 best_acc = config.baseline
 
-loss_fn = nCrossEntropyLoss()
+loss_fn = torch.nn.BCEWithLogitsLoss(reduce=False)
 
 for epoch in range(config.epochs):
     for i, (input, target) in enumerate(train_data_iterator()):
