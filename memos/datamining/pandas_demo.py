072a72b7951ecbaf8d25ff838dece62aef01f6b3
siriusdemon <mory2016@126.com>
2018-10-12 16:08:54 +0800

    pandas demo

--- memos/datamining/pandas_demo.py
+++ memos/datamining/pandas_demo.py
@@ -1,15 +1,15 @@
 import pandas as pd
-
+import numpy as np
 
 index = ['Mory', 'Ann']
 columns = ['Windy', 'Sunny', 'Snowy', 'Thundery', 'Soild', 'Lighting']
 data = {
-    'Mory': ['0.0', '0.0', '0.0', '0.0', '0.0', '0.0'],
-    'Ann': ['1.0', '1.0', '1.0', '1.0', '1.0', '1.0'],
+    'Mory': [2.0, 4.0, 6.0, 7.0, 6.0, 5.0],
+    'Ann': [1.0, 5.0, 1.0, 1.0, 1.0, 1.0],
 }
 
 
-df = pd.DataFrame(index=index, columns=columns)
+df = pd.DataFrame(index=index, columns=columns, dtype=np.float64)
 
 for (k, v) in data.items():
     df.T[k] = v
