26bf852d898812a3a31e7cd2a56f81c7bd6dff46
siriusdemon <mory2016@126.com>
2018-10-15 19:10:16 +0800

    take care of yourself

--- memos/datamining/pandas_demo.py
+++ memos/datamining/pandas_demo.py
@@ -31,3 +31,19 @@ mory = df_surpass.iloc[0]
 
 df_surpass.loc[4] = 'Know myself', 3.5, 'Demon'
 print(df_surpass)
+
+df_surpass.sort_values(by='Level', ascending=False)
+
+surpass_type = pd.Series(
+    data=['light', 'demon', 'snow', np.nan],
+    index=[2, 1, 3, 4]
+)
+
+df_surpass['SType'] = surpass_type
+print(df_surpass)
+
+df_surpass['SType'].fillna('ordinary', inplace=True)
+print(df_surpass)
+
+df_surpass['Level'] = df_surpass['Level'].map(lambda x: min(5, x+1))
+print(df_surpass)
