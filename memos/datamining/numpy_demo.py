3485b3edd4d5d421f02a75e87c12fb4bd5134175
siriusdemon <mory2016@126.com>
2018-10-15 19:32:47 +0800

    add comment to numpy demo

--- memos/datamining/numpy_demo.py
+++ memos/datamining/numpy_demo.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+# numpy demo: concatenate two array
 d1 = np.array([
     [1, 2, 3],
     [11, 12, 13],
@@ -14,3 +14,6 @@ dc = np.concatenate([d1, d2], axis=0)
 dc_ = np.hstack([d1, d2])
 dv = np.concatenate([d1, d2], axis=1)
 dv_ = np.vstack([d1, d2])
+
+
+# 
