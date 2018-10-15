ed6fe46890f1743df99d5965d8b8193a5a592551
siriusdemon <mory2016@126.com>
2018-10-15 19:00:58 +0800

    pandas demo

--- memos/datamining/pandas_demo.py
+++ memos/datamining/pandas_demo.py
@@ -16,3 +16,18 @@ for (k, v) in data.items():
 
 print(df)
     
+
+######## demo2
+data = {
+    'Name': ['Mory', 'Ann', 'Jenny'],
+    'Dream': ['Become a leader', 'Maybe world will enlightened', 'Everyone in happiness'],
+    'Level': [2.0, 5.0, 2.5]
+}
+
+df_surpass = pd.DataFrame(data=data, index=[1, 2, 3])
+
+ann = df_surpass.iloc[1]
+mory = df_surpass.iloc[0]
+
+df_surpass.loc[4] = 'Know myself', 3.5, 'Demon'
+print(df_surpass)
