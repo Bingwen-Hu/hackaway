16027d4974d19bc4f6b0515a0882421d72316ff6
siriusdemon <mory2016@126.com>
2018-10-24 17:50:43 +0800

    there is still a lot to learn and the best way to learn is to use it

--- memos/datamining/sklearn_demo.py
+++ memos/datamining/sklearn_demo.py
@@ -37,3 +37,25 @@ sample = np.array([
 scaler = StandardScaler()
 std_data = scaler.fit_transform(sample)
 org_data = scaler.inverse_transform(std_data)
+
+
+
+# toy model
+from sklearn.ensemble import RandomForestRegressor
+X = np.array([
+    [0, 1, 1.2, 2.3],
+    [0, 1, 3.4, 5.6],
+    [1, 0, 0.7, 1.3],
+])
+
+y = np.array([
+    [1.0, 1.0], 
+    [2.5, 2.0],
+    [0.3, .2],
+])
+
+rf = RandomForestRegressor()
+rf.fit(X, y)
+rf.predict([
+    [0, 1, 1., 2.]
+])
