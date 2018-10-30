d5f1cde332e00264515f8c5239892f78d3d36226
siriusdemon <mory2016@126.com>
2018-10-30 18:08:37 +0800

    refactor coefficient

--- books/master/statistic/coefficient.py
+++ books/master/statistic/coefficient.py
@@ -1,6 +1,7 @@
 import numpy as np
 import seaborn as sns
 
+
 def coefficient(X, Y):
     """ Pearson correlation coefficient
                          n * sum(XY) - sum(X)sum(Y) 
@@ -15,7 +16,7 @@ def coefficient(X, Y):
         return n * sum_of_square - square_of_sum
     n = X.shape[0]
     numerator = n * np.sum(X * Y) - np.sum(X) * np.sum(Y)
-    denominator = np.sqrt(deno(X) * deno(Y))
+    denominator = np.sqrt(deno(X)) * np.sqrt(deno(Y)) # avoid overflow
     return numerator / denominator
 
     
@@ -25,6 +26,22 @@ def demo():
     Y = np.array([3, 2, 6, 5, 3, 6, 5, 4, 4, 5])
     R_xy = coefficient(X, Y)
     sns.scatterplot(x=X, y=Y)
+
+def correlation_coefficient_test():
+    marriage_quality = np.array([
+        76, 81, 78, 76, 76, 78, 76, 78, 98, 88, 76, 66, 44, 
+        67, 65, 59, 87, 77, 79, 85, 68, 76, 77, 98, 99, 98, 
+        87, 67, 78,
+    ])
+    parent_children_relation = np.array([
+        43, 33, 23, 34, 31, 51, 56, 43, 44, 45, 32, 33, 28,
+        39, 31, 38, 21, 27, 43, 46, 41, 41, 48, 56, 55, 45, 
+        68, 54, 33,
+    ])
+    coefficient_ = coefficient(marriage_quality, parent_children_relation)
+    # left the same problem with anova.py
+    # never mind
+    return coefficient_
 
 if __name__ == '__main__':
     demo()
