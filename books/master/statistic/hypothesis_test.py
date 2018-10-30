425e8aa08ec0ac7ffac1cccfa32bca9c48c85361
siriusdemon <mory2016@126.com>
2018-10-30 11:29:23 +0800

    hypothesis test theory part

---
+++ books/master/statistic/hypothesis_test.py
@@ -0,0 +1,10 @@
+# null hypothesis: two variable is independent
+# null hypothesis is the baseline
+# because utill you show prove the difference exist or else
+# you can only believe they are the same.
+
+
+# nondirectional research hypothesis 
+# H1: X_9 != X_12
+# directional research hypothesis
+# H1: X_9 > X_12
