963feb64b7de62e6c969c2abe5c6d4f6c0ac1bd9
siriusdemon <mory2016@126.com>
2018-11-07 14:38:13 +0800

    calculus

---
+++ books/master/math/calculus.py
@@ -0,0 +1,18 @@
+"""
+            x
+y = ln tan ---  -  cos x * ln tan x
+            2 
+
+     (tan x/2)'                                 (tan x)'
+y' = ---------- - [ sin x * ln tan x + cos x * --------- ]
+      tan x/2                                    tan x
+
+其中
+               1
+(tan x)' = -----------
+            (cos x)^2
+              1
+(tan x/2)' = --- (cos x/2)^2   
+              2 
+
+"""
