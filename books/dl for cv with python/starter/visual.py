95a730910de34371290210c40a3efa5d6f452ef8
siriusdemon <mory2016@126.com>
2018-11-14 20:19:49 +0800

    make it show

--- books/dl for cv with python/starter/visual.py
+++ books/dl for cv with python/starter/visual.py
@@ -3,4 +3,4 @@ from keras.utils import plot_model
 
 
 model = LeNet.build(28, 28, 1, 10)
-plot_model(model, to_file='output/lenet.png', show_shapes=True)
+plot_model(model, to_file='lenet.png', show_shapes=True)
