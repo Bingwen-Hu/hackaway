da1a4571cf84165cf37339aad6ad904a4bd8c6e0
siriusdemon <mory2016@126.com>
2018-11-20 11:43:01 +0800

    opencv contour example

--- memos/opencv/contour.py
+++ memos/opencv/contour.py
@@ -1,20 +1,27 @@
 import cv2
 import numpy as np
-
+from uuid import uuid1
 
 def findContour(imagepath):
     img = cv2.imread(imagepath)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('image', img)
+
 
     blurred = cv2.GaussianBlur(gray, (11, 11), 0)
     edged = cv2.Canny(blurred, 30, 150)
-    cv2.imshow('Edges', edged)
+
 
     (_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     alphebat = img.copy()
     cv2.drawContours(alphebat, cnts, -1, (0, 255, 0), 2)
-    cv2.imshow('Coins', alphebat)
-    cv2.waitKey(0)
 
+    for i, c in enumerate(cnts):
+        (x, y, w, h)  = cv2.boundingRect(c)
+        crop = img[y:y+h, x:x+w]
+        cv2.imwrite(f"{uuid1()}.png", crop)
+
+
+if __name__ == '__main__':
+    imgpath = 'seccode_1.png'
+    findContour(imgpath)
