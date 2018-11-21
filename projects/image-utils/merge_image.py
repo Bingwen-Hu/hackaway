1e335312238524d8c86122f13d2f8f3104411cee
siriusdemon <mory2016@126.com>
2018-11-21 16:47:45 +0800

    merge_image.py not very useful

--- projects/image-utils/merge_image.py
+++ projects/image-utils/merge_image.py
@@ -4,9 +4,8 @@ import numpy as np
 from PIL import Image
 import glob
 from collections import Counter
+from imutils import paths
 
-imagedir = 'E:/captcha-data/img500/temp'
-imagepaths = glob.glob(f'{imagedir}/*') 
 
 def collect_images_numpy(imagepaths):
     for impath in imagepaths:
@@ -19,24 +18,26 @@ def extract_images(datalist):
     d = datalist[0]
     try:
         row, col, channel = d.shape
-        new_image = np.ndarray((row, col, channel), dtype=np.uint8)
+        new_image = np.zeros((row, col, channel), dtype=np.uint8)
     except:
         row, col = d.shape
-        new_image = np.ndarray((row, col), dtype=np.uint8)
+        new_image = np.zeros((row, col), dtype=np.uint8)
     
     for i in range(row):
         for j in range(col):
-            dlist = [data[i][j] for data in datalist]
-            slist = [sum(d) for d in dlist]
-            s, _ = Counter(slist).most_common(1)[0]
-            index = slist.index(s)
-            new_image[i][j] = dlist[index]
+            dlist = [tuple(data[i][j]) for data in datalist]
+            s, _ = Counter(dlist).most_common(1)[0]
+            new_image[i][j] = s
     return new_image
     
 
 
 
 if __name__ == '__main__':
+    
+    imagedir = '/home/jenny/Downloads/hk500_tag/lightg'
+    imagepaths = paths.list_images(imagedir)
+
     datalist = list(collect_images_numpy(imagepaths))
     r = extract_images(datalist)
     new = Image.fromarray(r)
