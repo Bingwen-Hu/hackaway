6bc816ed99c6698d42fae23f3fa44295822f976c
siriusdemon <mory2016@126.com>
2018-11-23 20:36:28 +0800

    standalone scip lisp script

---
+++ books/scip/scip.lisp
@@ -0,0 +1,24 @@
+;; Common lisp come back
+
+(+ 1 1)
+(+ 137 439)
+(/ 10 5)
+(+ 2.5 10)
+(+ 21 24 14 0)
+(+ (* 3
+      (+ (* 2 4)
+         (+ 3 5)))
+   (+ (- 10 5)
+      7))
+
+
+(setq size 2)
+(* 4 size)
+
+; (defparameter pi 3.14159) special constant
+(setq radius 10)
+(* pi (* radius radius))
+
+(setq circumference (* 2 pi radius))
+
+
