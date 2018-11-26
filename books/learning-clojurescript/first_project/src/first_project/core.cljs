fc6d3d92b35095cbe07bf7d882c3addf59714609
siriusdemon <mory2016@126.com>
2018-11-26 20:24:30 +0800

    up and running

--- books/learning-clojurescript/first_project/src/first_project/core.cljs
+++ books/learning-clojurescript/first_project/src/first_project/core.cljs
@@ -1,2 +1,7 @@
-(ns first-project.core)
+(ns first-project.core
+    (:require [clojure.browser.repl :as repl]))
+
+(defonce conn
+  (repl/connect "http://localhost:9000/repl"))
+
 (js/alert "Hello world!")
