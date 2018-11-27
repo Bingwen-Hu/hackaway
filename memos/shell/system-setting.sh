df44ba5f4fe81286fd54ee1eed2a2c23267d17fe
siriusdemon <mory2016@126.com>
2018-11-27 12:46:44 +0800

    add tree

--- memos/shell/system-setting.sh
+++ memos/shell/system-setting.sh
@@ -8,13 +8,20 @@ set timeout 10
 # send "2Foralfv\r"
 
 # spawn sudo zypper update zypper
+# expect "*(y):"
+# send "\r"
 # spawn sudo zypper install git emacs sbcl rlwarp
+# expect "*(y):"
+# send "\r"
+
 
 # # vscode 
 # spawn sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 # spawn sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
 # spawn sudo zypper refresh
 # spawn sudo zypper install code
+# expect "*(y):"
+# send "\r"
 
 # rust
 # spawn curl https://sh.rustup.rs -sSf | sh
