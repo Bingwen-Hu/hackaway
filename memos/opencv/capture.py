a82a1393691315f3804674458d3caae44996869b
siriusdemon <mory2016@126.com>
2018-12-18 17:47:58 +0800

    linux test pass

--- memos/opencv/capture.py
+++ memos/opencv/capture.py
@@ -9,21 +9,13 @@ if cap.isOpened():
 else:
     cap.open(0)
     print('whether open? {}'.format(cap.isOpened()))
-"""
+
 while(True):
     # capture frame by frame
     ret, frame = cap.read()
     
     # our operations on the frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+    cv2.imshow('camera', frame)
     # Display the resulting frame
-    cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
-    # when everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
-"""
-
