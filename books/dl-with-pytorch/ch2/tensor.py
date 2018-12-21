49a7f96796ac36829e63b5dca4badd1aecacadd1
siriusdemon <mory2016@126.com>
2018-12-21 16:32:48 +0800

    tensor in pytorch

--- books/dl-with-pytorch/ch2/tensor.py
+++ books/dl-with-pytorch/ch2/tensor.py
@@ -9,3 +9,12 @@ x.size()
 temp = torch.FloatTensor([23, 24, 25, 5, 33, 42, 0, 2])
 temp.size()
 
+# matrix
+import numpy as np
+
+data = np.random.randn(100, 20)
+tensor = torch.from_numpy(data)
+tensor.size()
+
+# back to numpy
+data_ = tensor.numpy()
