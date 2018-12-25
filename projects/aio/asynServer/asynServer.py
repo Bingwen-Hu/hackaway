1a1a04d93b0b855ea903ebf3cc037c099a7aed20
siriusdemon <mory2016@126.com>
2018-12-25 16:27:19 +0800

    class view as route

--- projects/aio/asynServer/asynServer.py
+++ projects/aio/asynServer/asynServer.py
@@ -2,24 +2,29 @@ from aiohttp import web
 import aiohttp_jinja2
 import jinja2
 
+
+routes = web.RouteTableDef()
+
+@routes.get('/')
 async def hello(request):
     return web.Response(text='Hello world!')
 
-
+# @routes.get('/{name}')
 async def passvar(request):
     name = request.match_info.get('name', 'unknown')
     return web.json_response({'name': name})
 
+@routes.view('/login')
 class LoginView(web.View):
         
-    async def post_login(self):
-        data = await self.post()
+    async def post(self):
+        data = await self.request.post()
         login = data['login']
         password = data['password']
         return web.json_response({'login': login, 'password': password})
 
     @aiohttp_jinja2.template('login.html')
-    async def get_login(self):
+    async def get(self):
         return {}
 
 
@@ -28,8 +33,5 @@ class LoginView(web.View):
 async def face_server():
     app = web.Application()
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
-    app.router.add_get('/', hello)
-    # app.router.add_get('/{name}/{ann}', passvar)
-    app.router.add_get('/login', LoginView.get_login)
-    app.router.add_post('/login', LoginView.post_login)
+    app.router.add_routes(routes)
     return app
