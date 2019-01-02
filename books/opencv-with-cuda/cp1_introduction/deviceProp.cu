83109268e5ffc4762996fcb0afd5a0d592c185de
siriusdemon <mory2016@126.com>
2019-01-02 10:34:57 +0800

    device prop

--- books/opencv-with-cuda/cp1_introduction/deviceProp.cu
+++ books/opencv-with-cuda/cp1_introduction/deviceProp.cu
@@ -12,8 +12,21 @@ int main(){
     int runtime_version;
     cudaDriverGetVersion(&driver_version);
     cudaRuntimeGetVersion(&runtime_version);
-    printf("\nCUDA driver Version / Runtime Version %i / %i", 
-            driver_version, runtime_version);
-    
+    printf("\nCUDA driver Version / Runtime Version %.3g / %.3g", 
+            driver_version/1000.0, runtime_version/1000.0);
+
+    // thread property
+    printf("\nMax number of threads per multiprocessor: %d\n", 
+            device_property.maxThreadsPerMultiProcessor);
+    printf("\nMax number of threads per block: %d\n",
+            device_property.maxThreadsPerBlock);
+    printf("\nMax dimension size of a thread block (x,y,z): (%d,%d,%d)\n",
+            device_property.maxThreadsDim[0],
+            device_property.maxThreadsDim[1],
+            device_property.maxThreadsDim[2]);
+    printf("\nMax dimension size of a grid size (x,y,z): (%d,%d,%d)\n",
+            device_property.maxGridSize[0],
+            device_property.maxGridSize[1],
+            device_property.maxGridSize[2]);
     return 0;
 }
