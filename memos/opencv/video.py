4a2d0b01173ad8a94c0f95d67f17ac05fefebf9a
siriusdemon <mory2016@126.com>
2019-01-04 17:09:00 +0800

    using opencv as video player

--- memos/opencv/video.py
+++ memos/opencv/video.py
@@ -1,24 +1,17 @@
-import numpy as np
 import cv2
+import time
 
-cap = cv2.VideoCapture("E:/Mory/0.mp4")
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-out = cv2.VideoWriter('E:/Mory/output.avi', fourcc, 20.0, (240, 120))
+cap = cv2.VideoCapture("/home/mory/Downloads/fengjiao.mp4")
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow("frame", frame)
     else:
         break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    time.sleep(0.025)
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
-
-
-# ffmpeg or gstreamer must be installed
-# something wrong!
