9184458043db91f858ec5b66522976d1b35aeb42
siriusdemon <mory2016@126.com>
2019-01-09 11:04:31 +0800

    using same fps as original video

--- memos/opencv/buildmp4.py
+++ memos/opencv/buildmp4.py
@@ -6,25 +6,26 @@ from os.path import basename
 def sort_helper(path):
     name = basename(path)
     name = name.lstrip('dance')
-    num = name.rstrip('_org.jpg')
+    num = name.rstrip('_skel.jpg')
     num = int(num)
     return num
 
 
-img_dir = "/home/mory/Downloads/output/org"
-video_path = "/home/mory/Downloads/org.avi"
+img_dir = "/home/mory/Downloads/output"
+video_path = "/home/mory/Downloads/dance.avi"
+# use for fps
+org_video_path = "/home/mory/Downloads/dance.mp4"
 
-fps = 30
+org = cv2.VideoCapture(org_video_path)
+fps = org.get(cv2.CAP_PROP_FPS)
+imgsize = int(org.get(cv2.CAP_PROP_FRAME_WIDTH)), int(org.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 
 images = [os.path.join(img_dir, f) for f in os.listdir(img_dir)]
 images = sorted(images, key=sort_helper)
 
-num = len(images)
-
-height, width = cv2.imread(images[0]).shape[:2]
-
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-videowriter = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
+videowriter = cv2.VideoWriter(video_path, fourcc, fps, imgsize)
 
 
 for img in images:
@@ -32,3 +33,4 @@ for img in images:
     videowriter.write(frame)
 
 videowriter.release()
+org.release()
