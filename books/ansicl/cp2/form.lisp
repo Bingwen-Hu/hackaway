a67a1b1842880a6048297fa18770bdef98d4c48f
siriusdemon <mory2016@126.com>
2019-01-10 19:56:12 +0800

    update

--- books/ansicl/cp2/form.lisp
+++ books/ansicl/cp2/form.lisp
@@ -91,3 +91,98 @@
 ; read in
 (let ((name (read)))
   (format t "my name is ~a" name))
+
+(defun askem (string)
+  (format t "~A" string)
+  (read))
+
+;;; 2.10 variables
+
+; local variable
+; (let ((var val)*) (body)*)
+(let () ())
+(let ((x 1)
+      (y 2))
+  (+ x y))
+
+(defun ask-number () 
+  (format t "Please enter a number. ")
+  (let ((val (read)))
+    (if (numberp val)
+        val 
+        (ask-number))))
+
+; global parameters
+(defparameter *glob* 42)
+(defconstant limit (+ *glob* 1))
+
+; check if the name is bounded
+(boundp 'limit)
+
+;;; 2.11 assignment
+
+; general assignment
+; create global variable implicitly
+(setf *mory* 98)
+(let ((n 10))
+  (setf n 2)
+  n)
+
+; assign a position
+(let ((x '(A B C)))
+  (setf (car x) 'Ann)
+  x)
+
+(setf a 'a
+      b 'b
+      c 'c)
+
+
+;;; 2.12 functional programming
+(setf lst '(c a r a t))
+(remove 'a lst)
+
+(defun mory-remove (obj lst)
+  "recurse process"
+  (if (null lst)
+      nil
+      (if (eql (car lst) obj)
+          (mory-remove obj (cdr lst))
+          (cons (car lst) (mory-remove obj (cdr lst))))))
+
+(defun mory-remove-iterate (obj lst)
+  (labels ((remove-iter (obj lst ret)
+             (cond ((null lst) ret)
+                   ((eql (car lst) obj) 
+                    (remove-iter obj (cdr lst) ret))
+                   (t 
+                    (remove-iter obj (cdr lst) (cons (car lst) ret))))))
+    (nreverse (remove-iter obj lst))))
+
+;;; 2.13 iteration
+; (do ((variable initial update)) (end-condition return-value) body*)
+(defun show-squares (start end)
+  (do ((i start (+ i 1)))
+      ((> i end) 'done)
+    (format t "~a ~a~%" i (* i i))))
+
+(defun mory-length (lst)
+  (let ((len 0))
+    (dolist (- lst)
+      (setf len (1+ len)))
+    len))
+
+(defun mory-length-recursive (lst)
+  (if (null lst)
+      0
+      (+ 1 (mory-length-recursive (cdr lst)))))
+
+(defun mory-length-iterative (lst)
+  (labels ((length-iter (lst len)
+             (if (null lst)
+                 len
+                 (length-iter (cdr lst) (1+ len)))))
+    (length-iter lst 0)))
+
+
+;;; 2.14 functions as objects
