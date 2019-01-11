36080c7af6bb723bf8445aaead0a4b6031d7e870
siriusdemon <mory2016@126.com>
2019-01-11 16:41:04 +0800

    buggy video

--- memos/opencv/video.py
+++ memos/opencv/video.py
@@ -1,17 +1,49 @@
 import cv2
 import time
+import numpy as np
 
-cap = cv2.VideoCapture("/home/mory/Downloads/org.avi")
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-        cv2.imshow("frame", frame)
-    else:
-        break
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    time.sleep(0.040)
+def single_show():
+    cap = cv2.VideoCapture("/home/mory/Downloads/org.avi")
 
-cap.release()
-cv2.destroyAllWindows()
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow("frame", frame)
+        else:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        time.sleep(0.040)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+import time
+def multi_show(writepath=None):
+    cap1 = cv2.VideoCapture('/home/mory/Downloads/mory.mp4')
+    cap2 = cv2.VideoCapture('/home/mory/Downloads/dance.avi')
+    fps = cap1.get(cv2.CAP_PROP_FPS)
+
+    imgsize = int(cap1.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    videowriter = cv2.VideoWriter(writepath, fourcc, fps, imgsize)
+
+    while True:
+        ret1, frame1 = cap1.read()
+        ret2, frame2 = cap2.read()
+        if ret1 == ret2 == True:
+            concat = np.hstack([frame2, frame1, frame2])
+            cv2.imshow('frame', concat)
+            videowriter.write(concat)
+        else:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        time.sleep(0.5/fps) 
+    videowriter.release()
+    cap1.release()
+    cap2.release()
+    cv2.destroyAllWindows()
+
+multi_show('/home/mory/Downloads/concat.avi')
