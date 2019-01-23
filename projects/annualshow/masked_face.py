07a602ef20a87b366be318a8a9a0b45065091a42
siriusdemon <mory2016@126.com>
2019-01-23 20:15:33 +0800

    refactor code

--- projects/annualshow/masked_face.py
+++ projects/annualshow/masked_face.py
@@ -1,23 +1,22 @@
+import os
+import time
+import argparse
+
+# third party
+import cv2
+import numpy as np
 import face_recognition
 from imutils import paths
-import os
-from os.path import basename
-import numpy as np
-import requests
-import cv2
-import time
-import matplotlib.pyplot as plt
 
-# external
+# local module
 from yolo import objectapi
 
 
+# global variable
 DATA_DIR = './dataset/images'
-
 NAME = 0
 DEPAT = 1
 JOB = 2
-# 一句话描述
 INTRO = 3
 
 INFO = {
@@ -34,11 +33,11 @@ INFO = {
     'unknown': ['unknown', 'unknown', 'unknown', 'unknown'],
 }
 
+
 def build_facedb(dirpath:str):
     """ build a face database as following:
     Args:
         dirpath: known face database, assume subdir name is the name of person
-
     Returns:
         key-value face database
     """
@@ -55,12 +54,14 @@ def build_facedb(dirpath:str):
         except:
             print('Could not find face!')
         else:
-            db_infos.append(basename(imgpath).split('.')[0])
+            db_infos.append(os.path.basename(imgpath).split('.')[0])
     return db_embed, db_infos
 
+# build face database
 DB_EMBED, DB_LABELS = build_facedb(DATA_DIR)
 
 
+# for person bbox detection
 def yolo_detect(img):
     results = objectapi.detect(img)
     results = [r['bbox'] for r in results if r['label'] == 'person']
@@ -68,10 +69,9 @@ def yolo_detect(img):
     return results
 
 
+# img_mask for final show
+# img_dark for face recognition show
 def region_mask(img, bbox):
-    """
-    return masked image and crop image for face recognition
-    """
     img_mask = img.copy()
     img_dark = img.copy()
     size = img.shape[:2]
@@ -98,27 +98,25 @@ def recognize(img:np.array, trick=None):
         min_index = np.argmin(distances)
         min_distance = distances[min_index]
         # threshold
-        label = DB_LABELS[min_index] if min_distance < 0.60 else 'unknown'
+        label = DB_LABELS[min_index] if min_distance < 0.70 else 'unknown'
         if trick and label != 'unknown':
             label = trick
         return {'bbox': [bbox[3], bbox[0], bbox[1], bbox[2]], 'label': label, 'distance': min_distance, 'info': INFO[label]}
     results = list(map(distance_helper, encodings, locations))
-    # results = sorted(results, key=lambda x: x['distance'])
     return results[0]
+
 
 def create_info(height, width, infos):
     label = infos['label']
     distance = infos['distance']
-    prob = min((1 - distance) + 0.2, 0.999) * 100
+    prob = min((1-distance), 0.999) * 100
     info = infos['info']
-    # build image path and read in
+    # build image path and read image
     if label == 'unknown':
         img = cv2.imread('unknown.jpg')
-        print('read unknown.jpg')
     else:
         path = os.path.join(DATA_DIR, f"{label}.jpg")
         img = cv2.imread(path)
-        print('read {}'.format(path))
     # resize to width when keeping height:width ratio by center crop
     img = resize(img, width)
     # create a canvas
@@ -126,32 +124,34 @@ def create_info(height, width, infos):
     canvas[0:width, 0:width, :] = img[:, :, :]
 
     def putText(canvas, info):
-        "Capture: `width`"
         from PIL import Image, ImageFont, ImageDraw
         canvas = cv2.cvtColor(canvas, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(canvas)
+        # support Simplified Chinese Font
         font = ImageFont.truetype('/home/mory/.mory/font/msyh.ttc', 40)
         draw = ImageDraw.Draw(img)
         # face difference
-        draw.text((10, width + 60), font=font, text=f"Probability: ", fill="#ffaaee")
-        draw.text((10, width + 120), font=font, text=f"{prob:.3f}%", fill="#ffaa00")
+        draw.text((10, width + 0), font=font, text=f"Probability: ", fill="#ffaaee")
+        draw.text((10, width + 60), font=font, text=f"{prob:.1f}%", fill="#ffaa00")
         # name
-        draw.text((10, width + 180), font=font, text=f"Name: ", fill="#ffaaee")
-        draw.text((10, width + 240), font=font, text=f"{info[NAME]}", fill="#ffaa00")
+        draw.text((10, width + 120), font=font, text=f"Name: ", fill="#ffaaee")
+        draw.text((10, width + 180), font=font, text=f"{info[NAME]}", fill="#ffaa00")
         # department
-        draw.text((10, width + 300), font=font, text=f"Department: ", fill="#ffaaee")
-        draw.text((10, width + 360), font=font, text=f"{info[DEPAT]}", fill="#ffaa00")
+        draw.text((10, width + 240), font=font, text=f"Department: ", fill="#ffaaee")
+        draw.text((10, width + 300), font=font, text=f"{info[DEPAT]}", fill="#ffaa00")
         # Job title
-        draw.text((10, width + 420), font=font, text=f"Job: ", fill="#ffaaee")
-        draw.text((10, width + 480), font=font, text=f"{info[JOB]}", fill="#ffaa00")
+        draw.text((10, width + 360), font=font, text=f"Job: ", fill="#ffaaee")
+        draw.text((10, width + 420), font=font, text=f"{info[JOB]}", fill="#ffaa00")
         # description
-        draw.text((10, width + 540), font=font, text=f"Description: ", fill="#ffaaee")
-        draw.text((10, width + 600), font=font, text=f"{info[INTRO]}", fill="#ffaa00")
+        draw.text((10, width + 480), font=font, text=f"Description: ", fill="#ffaaee")
+        draw.text((10, width + 540), font=font, text=f"{info[INTRO]}", fill="#ffaa00")
         return np.array(img)
     img = putText(canvas, info)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     return img
 
+
+# resize image by center crop
 def resize(img, width):
     h, w = img.shape[:2]
     if h > w:
@@ -162,10 +162,12 @@ def resize(img, width):
         img = img[:, start_w:start_w+h, :]
     return cv2.resize(img, (width, width))
 
+
 def draw_facebbox(img, bbox):
     if bbox is not None:
         img = cv2.rectangle(img, (bbox[0]-20, bbox[1]-20), (bbox[2]+20, bbox[3]+20), (128, 255, 128), 3)
     return img
+
 
 def append_info(img, img_info):
     width = 4
@@ -176,60 +178,54 @@ def append_info(img, img_info):
     return combine
 
 
-trick_labels = {
-    0: 'junguang',
-    1: 'kejian',
-    2: 'guangyi',
-}
+def parse_args():
+    import argparse
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--video', '-v', required=True)
+    argparser.add_argument('--dir', '-d', required=True)
+    argparser.add_argument('--fmax', '-m', required=True)
+    args = argparser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
     # init
+    t = time.time()
     bbox_index = 0
-    frame_max = 244
     frame_cnt = 0
     total_cnt = 0
-    bboxes_num = 3
-    cap = cv2.VideoCapture('/home/mory/data/face/metoo2_938.mp4')
-
-    # for debug
-    for i in range(720):
-        ret, img = cap.read()
-        frame_cnt += 1
-        total_cnt += 1
-        bbox_index += 1
-        if bbox_index == bboxes_num:
-            bbox_index -= 1
-        print('process: %d, bbox_index: %d\r' % (total_cnt, bbox_index))
-
+    frame_max = int(args.fmax)
+    cap = cv2.VideoCapture(f'/home/mory/{args.video}')
     # procedure1: read in raw image -> image
     while True:
         ret, img = cap.read()
         if not ret:
             break
-        # img = resize(img, 1000)
+        img = resize(img, 1000)
         # procedure2: yolo detect person -> person-bbox
         bboxes = yolo_detect(img)
         bboxes_num = len(bboxes)
+        if not bboxes_num:
+            print('empty frame')
+            continue
         # procedure3: masked just one person out -> masked-image
-        try:
-            bbox = bboxes[bbox_index]
-        except:
-            cv2.imwrite('bug_%d_%d.png' % (total_cnt, bbox_index), img)
-        # masked, darked = region_mask(img, bbox)
+        if bbox_index >= bboxes_num:
+            continue
+        bbox = bboxes[bbox_index]
+        masked, darked = region_mask(img, bbox)
         # procedure4: faceapi detect the person -> label, infos
-        # infos = recognize(darked, None)
+        infos = recognize(darked, None)
         # procedure5.1: create info -> info-image
-        # img_info = create_info(img.shape[0], 300, infos)
+        img_info = create_info(img.shape[0], 300, infos)
         # procedure5.2: draw face bbox -> face-image
-        # img_face = draw_facebbox(masked, infos['bbox'])
+        img_face = draw_facebbox(masked, infos['bbox'])
         # img_face = masked
         # procedure6: combine masked-image and info-image -> END
-        # img = append_info(img_face, img_info)
+        img = append_info(img_face, img_info)
         # cv2.imshow("test", img)
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #     break
-        # cv2.imwrite('{}.png'.format(total_cnt), img)
+        cv2.imwrite('new/{}/{}.png'.format(args.dir, total_cnt), img)
 
         # monitor frame count
         frame_cnt += 1
@@ -239,5 +235,5 @@ if __name__ == "__main__":
             frame_cnt = 0
             bbox_index += 1
             if bbox_index == bboxes_num:
-                bbox_index -= 1
-        print('\rprocess: %d, bbox_index: %d' % (total_cnt, bbox_index))
+                bbox_index = 0
+        print(f'process image {total_cnt} cost: {time.time() - t}s')
