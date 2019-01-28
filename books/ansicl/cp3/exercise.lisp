62b98f1a28bfb82bc51ec656c26a29f794de4ca2
siriusdemon <mory2016@126.com>
2019-01-28 10:46:28 +0800

    common lisp exercise 3 finish

--- books/ansicl/cp3/exercise.lisp
+++ books/ansicl/cp3/exercise.lisp
@@ -15,3 +15,71 @@
     (let ((ret (occur lst nil)))
       (sort ret #'> :key #'cdr))))
 
+(occurrences '(a b a d a c d c a))
+
+;;; e4
+(member '(a) '((a) (b)))
+(member '(a) '((a) (b)) :test #'equal)
+
+
+;;; e5
+(defun pos+ (lst)
+  "iteration"
+  (let ((index 0))
+    (dolist (int lst lst)
+      (setf (nth index lst) (+ index int))
+      (incf index))))
+
+(defun pos+ (lst)
+  "recursion"
+  (labels ((pos-add (to-add lst ret)
+             (if (null lst)
+                 ret
+                 (pos-add (1+ to-add) (cdr lst) 
+                          (cons (+ (car lst) to-add) ret)))))
+    (nreverse (pos-add 0 lst nil))))
+
+(defun pos+ (lst)
+  "USING MAPCAR"
+  (labels ((build-index (index lst ret)
+             (if (null lst)
+                 ret
+                 (build-index (1+ index) (cdr lst) (cons index ret)))))
+    (let ((index-list (nreverse (build-index 0 lst nil))))
+      (mapcar #'+ lst index-list))))
+
+; define your own cons, list, length, member
+(defun mory-cons (a b)
+  `(,a . ,b))
+
+(defun mory-list (&rest lst)
+  (defun list-with-ret (lst ret)
+    (if (null lst)
+        ret
+        (list-with-ret (cdr lst) 
+                       (mory-cons (car lst) ret))))
+  (nreverse (list-with-ret lst nil)))
+
+(defun mory-length (lst)
+  (if (null lst)
+      0
+      (1+ (mory-length (cdr lst)))))
+
+(defun mory-member (a lst)
+  (if (null lst)
+      nil
+      (if (eql a (car lst))
+          lst
+          (mory-member a (cdr lst)))))
+
+; defina a function that takes a list and prints it in dot notation
+(defun showdots (lst)
+  (if (null lst)
+      (princ nil)
+      (progn 
+        (princ "(")
+        (princ (car lst))
+        (princ " . ")
+        (showdots (cdr lst))
+        (princ ")")
+        nil)))
