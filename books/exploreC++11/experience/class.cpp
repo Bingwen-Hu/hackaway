4f1ee74ba3eb8b04792fb105516dd2effba0f55f
siriusdemon <mory2016@126.com>
2019-02-10 15:19:06 +0800

    separate file for virtual function definition

--- books/exploreC++11/experience/class.cpp
+++ books/exploreC++11/experience/class.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 class work
 {
@@ -9,6 +10,11 @@ public:
         : id_{id}, title_{title} 
     {}
 
+    virtual void info() const {
+        using namespace std;
+        cout << "ID: " << id() << endl;
+        cout << "Title: " << title() << endl;
+    };
     // why const before return body
     std::string const& id() const { return id_; }
     std::string const& title() const { return title_; }
@@ -17,3 +23,39 @@ private:
     std::string id_;
     std::string title_;
 };
+
+
+class book : public work
+{
+public:
+    book() : work{}, author_{}, pubyear_{}
+    {}
+    book(book const&) = default;
+    book(std::string const& id, std::string const& title, std::string const& author, int pubyear) 
+        : work{id, title}, author_{author}, pubyear_{pubyear}
+    {}
+    std::string const& author() const { return author_; }
+    int pubyear() const { return pubyear_; }
+    void info() const override
+    {
+        using namespace std;
+        cout << left;
+        cout << setw(20) << "ID:" <<  this->id() << endl;
+        cout << setw(20) << "Title:" << this->title() << endl;
+        cout << setw(20) << "Author:" << this->author() << endl;
+        cout << setw(20) << "Public Year:" << this->pubyear() << endl;
+    }
+
+private:
+    std::string author_;
+    int pubyear_;
+};
+
+
+int main()
+{
+    book b{"bookid", "title of book", "author of book", 2010}; 
+    b.info();
+    work w{"workId", "title of work}"};
+    w.info();
+}
