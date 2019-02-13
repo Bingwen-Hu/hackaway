55c8bca9ffa69c94d42eda230a5cd4f437ba07c4
siriusdemon <mory2016@126.com>
2019-02-13 18:47:28 +0800

    start ABBA challenge

---
+++ projects/coding/algorithms/ABBA.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class ABBA
+{
+public:
+    string canObtain(string initial, string target);
+};
+
+string canObtain(string initial, string target)
+{
+    string str;
+    return str;
+}
+
+void test()
+{
+}
+
+int main()
+{
+    test();
+}
