116f4f56664e1513abc362a7dfc909620a585a3d
siriusdemon <mory2016@126.com>
2019-02-13 10:09:33 +0800

    not very fast

--- projects/topcoder/algorithms/ADPaper.cpp
+++ projects/topcoder/algorithms/ADPaper.cpp
@@ -1,8 +1,7 @@
-// function style 
-
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -43,6 +42,7 @@ public:
     ADPaper();
     ~ADPaper();
     string canBuild(vector<int> A);
+    string canBuild(vector<int> A, int fast);
 };
 
 ADPaper::ADPaper(){};
@@ -63,11 +63,35 @@ string ADPaper::canBuild(vector<int> A){
     }
 }
 
+string ADPaper::canBuild(vector<int> A, int fast)
+{
+    long long compare = 1; 
+    int len = A.size();
+    long long x = 0;
+    
+    for (int i = 0; i < len; i++){
+        x = A[i];
+        if (x >= compare) {
+            return "possible";
+        }
+        compare = compare << 1;
+        if ((i+1) == len) {
+            return "impossible";
+        }
+        A[i+1] += A[i] << 1;
+    }
+    return "impossible";
+}
+
+
 int main()
 {
     // test_canBuild();
     auto adp{ADPaper()};
     vector<int> A{0,3};
-    string s{adp.canBuild(A)};
+    string s{adp.canBuild(A, 1)};
+    cout << s << endl;
+    vector<int> D{0,0,0,0,0,0,0,3,2,0,0,5,0,3,0,0,1,0,0,0,5};
+    s = adp.canBuild(D, 1);
     cout << s << endl;
 }
