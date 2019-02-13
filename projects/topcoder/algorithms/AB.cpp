85df041b9ddb687bc57e37cc9701f80024c0625a
siriusdemon <mory2016@126.com>
2019-02-13 15:03:02 +0800

    pass one

--- projects/topcoder/algorithms/AB.cpp
+++ projects/topcoder/algorithms/AB.cpp
@@ -2,23 +2,38 @@
 #include <string>
 #include <iostream>
 
+using namespace std;
 
 class AB
 {
+public:
     string createString(int N, int K);
-}
+};
 
 string AB::createString(int N, int K)
 {
-    int maxA = N / 2;
-    int maxB = N - maxA;
-    int residual = maxA * maxB - K;     
+    int A = N / 2;
+    int B = N - A;
+    int residual = A * B - K;     
 
     if (residual < 0){
         return "";
     }
 
+    string&& str = "", strA, strB;
+    // exchange A and B totally
+    int B_to_start = residual / A;
+    str.assign(B_to_start, 'B');
+    strA.assign(A, 'A');
+    str.append(strA);
+    strB.assign(B-B_to_start, 'B');
+    str.append(strB);
 
+    // move forward certain steps
+    int B_to_step = B_to_start + A - residual % A;
+    str[B_to_step] = 'B';
+    str[B_to_start+A] = 'A';
+    return str;
 }
 
 void test(AB& ab)
@@ -28,5 +43,7 @@ void test(AB& ab)
 
 int main()
 {
-
+    AB ab{};
+    string str = ab.createString(10, 12);
+    cout << str << endl;
 }
