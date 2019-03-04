927de92c7c6aeaf81d8ab26e771586a1071396b4
siriusdemon <mory2016@126.com>
2019-03-04 10:27:58 +0800

    add show sample

--- projects/compet/explore.py
+++ projects/compet/explore.py
@@ -4,6 +4,10 @@ import json
 import pandas as pd
 from collections import Counter
 
+import cv2
+
+TRAIN_DIR = "data/round1_train/restricted/"
+
 
 def read_json(path: str):
     with open(path, 'r') as f:
@@ -17,6 +21,18 @@ def count(jsonfile: dict):
 def summary_image(filenames, filesizes, fileids):
     pass
 
+
+def crop_sample(annotations, images):
+    filename = images.file_name[4]
+    path = os.path.join(TRAIN_DIR, filename)
+    bbox = annotations[annotations.image_id==4]
+    bbox = bbox.bbox
+    img = cv2.imread(path)
+    for bb in bbox:
+        cv2.rectangle(img, (int(bb[0]), int(bb[1])), (int(bb[0]+bb[2]), int(bb[1]+bb[3])), 2)
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
@@ -44,3 +60,6 @@ if __name__ == '__main__':
     # size statical
     images['area'] = images.apply(lambda x: x.width * x.height, axis=1)
     print(images.describe())
+
+    # show sample
+    crop_sample(annotations, images)
