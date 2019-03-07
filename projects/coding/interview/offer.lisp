7c07fbc5f6f1204e096686dfd613c937db382e60
siriusdemon <mory2016@126.com>
2019-03-07 18:05:03 +0800

    lisp for exercise

--- projects/coding/interview/offer.lisp
+++ projects/coding/interview/offer.lisp
@@ -1,4 +1,27 @@
 ;;; input is list of ints
-;;; ints lie in [0, 1, ..., N-1]
-(defun duplicate (lst N)
-  ())
+;;; ints lie in [0, 1, ..., N-1
+(defvar N 40)
+(defun duplicate (lst)
+  (let ((hold (make-array N)))
+    (dolist (i lst)
+      (incf (aref hold i))
+      (if (> (aref hold i) 1)
+          (return t)))))
+
+(defun replace-space (str))
+
+(defun search-matrix)
+
+
+(defun reverse-print (lst)
+  "using recursive"
+  (if (not (null (rest lst)))
+      (reverse-print (rest lst)))
+  (print (first lst)))
+
+(defun reverse-print-stack (lst)
+  (let ((stack '()))
+    (dolist (e lst)
+      (push e stack))
+    (dolist (e stack)
+      (print e))))
