0d16d0b5f85d5f75ac35f043c4696a4d5cb665b7
siriusdemon <mory2016@126.com>
2019-03-15 19:20:43 +0800

    prepare to interview

--- projects/interview/offer/reversePrint.c
+++ projects/interview/offer/reversePrint.c
@@ -1,6 +1,6 @@
-// WARNING: BUGGY
 #include <stdio.h>
 #include <stdlib.h>
+
 
 typedef struct ListNode
 {
@@ -14,6 +14,16 @@ typedef struct List
     int length;
 } List;
 
+void printList(List*);
+
+List* makeList(){
+    List* list = (List*)malloc(sizeof(List));
+    list->head = (ListNode*)malloc(sizeof(ListNode));
+    list->length = 0;
+    list->head->next = NULL;
+    return list;
+}
+
 void addNode(List* list, int value) {
     ListNode* node = (ListNode*)malloc(sizeof(ListNode));
     node->value = value;
@@ -23,19 +33,22 @@ void addNode(List* list, int value) {
 }
 
 void printList(List* list){
-    ListNode* p = list->head;
-    while (p->next) {
+    ListNode* p = list->head->next;
+    while (p != NULL) {
         printf("%d ", p->value);
+        p = p->next;
     } 
+    printf("\n");
 }
 
 int main()
 { 
     int values[] = {10, 9, 23, 42, 19, 25};
-    List list;   
+    List* list = makeList();   
     for (int i = 0; i < 6; i++)
     {
-        addNode(&list, values[i]);
+        addNode(list, values[i]);
     }
-    printList(&list);
+    printList(list);
+    return 0;
 }
