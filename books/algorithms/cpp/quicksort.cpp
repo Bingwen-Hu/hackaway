d5b0e368e02c0467cba4ce71de00fdab6d6e7f9f
siriusdemon <mory2016@126.com>
2019-04-04 11:29:29 +0800

    add quicksort in cpp

--- books/algorithms/cpp/quicksort.cpp
+++ books/algorithms/cpp/quicksort.cpp
@@ -5,31 +5,49 @@
 using namespace std;
 
 
-int select_pivot(vector<int>& vec, int start, int end)
+void swap(vector<int>& vec, int i, int j) 
 {
-    // simple 
-    return start;
+    int t = vec.at(i);
+    vec[i] = vec.at(j);
+    vec[j] = t;
 }
 
 int partition(vector<int>& vec, int start, int end) 
 {
-    q = select_pivot(vec, start, end);
+    int x = vec.at(end-1);
+    int i = start;
 
-
-    return q;
+    for (int j = start; j < end - 1; j++) {
+        if (vec.at(j) < x) {
+            swap(vec, j, i);
+            i++;
+        }
+    }
+    swap(vec, i, end-1);
+    return i;
 }
 
 void quicksort(vector<int>& vec, int start, int end) 
 {
     if (start < end) {
-        q = partition(vec, start, end);
+        int q = partition(vec, start, end);
         quicksort(vec, start, q);
         quicksort(vec, q+1, end);
     }
 }
 
+void print(vector<int>& vec)
+{
+    for (int i : vec) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
 
 int main()
 {
-
+    vector<int> vec{2, 8, 12, 7, 9, 14, 5, 6, 16, 1, 3, 10};
+    quicksort(vec, 0, vec.size());
+    print(vec);
 }
