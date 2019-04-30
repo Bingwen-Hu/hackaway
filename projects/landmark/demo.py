2e4bff40440b3c6cd0df2be34191caf9b6ff4752
siriusdemon <mory2016@126.com>
2019-04-30 14:46:11 +0800

    add number

--- projects/landmark/demo.py
+++ projects/landmark/demo.py
@@ -16,13 +16,16 @@ def demo_detect(image:str):
         x = points[i] * (x2 - x1) + x1
         y = points[i+1] * (y2 - y1) + y1
         cv2.circle(img, (int(x), int(y)), 1, (128, 255, 255), 2)
+        cv2.putText(img, f"{i//2+1}", (int(x), int(y)), fontFace=cv2.FONT_HERSHEY_PLAIN, 
+            fontScale=1, color=(0, 255, 0), thickness=1), 
     cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
     cv2.imshow("image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    cv2.imwrite("results/timg.jpeg", img)
 
 if __name__ == '__main__':
     imgpath = 'images/timg.jpeg'
-    img = demo_show(imgpath)
-    cv2.imwrite("results/timg.jpeg", img)
-    # demo_detect(imgpath)
+    # img = demo_show(imgpath)
+    # cv2.imwrite("results/timg.jpeg", img)
+    demo_detect(imgpath)
