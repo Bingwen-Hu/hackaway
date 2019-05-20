fca2cc623fa2b8b686c2d7302c0b81729ccbaa5d
siriusdemon <mory2016@126.com>
2019-05-20 17:39:29 +0800

    more function about loop

--- books/TRPL/cp3_commons/for.rs
+++ books/TRPL/cp3_commons/for.rs
@@ -1,5 +1,4 @@
 // have a look at for
-
 fn main(){
     let array = [1, 2, 3, 4, 5];
 
@@ -7,10 +6,10 @@ fn main(){
         println!("The element of array is: {}", i);
     }
 
-
     test_while();
+    test_loop();
+    test_iter();
 }
-
 
 fn test_while() {
     // no differece except the condition
@@ -20,5 +19,23 @@ fn test_while() {
     while i >= 0 {
         println!("I just learn the grammer: {}", i);
         i = i - 1;
+    }
+}
+
+fn test_loop() {
+    // return value from loop
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("Counter return from loop {}, {}", counter, result);
+}
+
+fn test_iter() {
+    for number in (1..4).rev() {
+        println!("{}!", number);
     }
 }
