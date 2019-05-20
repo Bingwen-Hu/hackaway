c89fb8550c75714777e9d465d6b244f11c5acf48
siriusdemon <mory2016@126.com>
2019-05-20 16:24:00 +0800

    add shadow

--- books/TRPL/cp3_commons/variable.rs
+++ books/TRPL/cp3_commons/variable.rs
@@ -3,22 +3,27 @@
 fn main(){
     let mut x = 5;
     println!("The value of x is {}", x);
-
     x = 19;
     println!("The value of x is {}", x);
     
-
     test_constant();
+    shadow();
 }
-
-
 
 fn test_constant(){
     // declare in here is valid?
+    // type of const must be announced
+    // convention: All uppercase with underscore
     const MORY_CONSTANT: u32 = 100_000;
 
     println!("Mory has a constant {}", MORY_CONSTANT);
 }
 
-
-// Yes, const can be declared in any where.
+fn shadow(){
+    // shadow allow you to use the same name and perform some modify before 
+    // set some variable to inmmutable.
+    // and shadow allow us to change type while `mut` could not.
+    let x = 10;
+    let x = x * 2;
+    println!("In {}, value of x is {}", "THIS FILE", x);
+}
