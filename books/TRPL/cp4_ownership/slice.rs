1e8361726d5d0afd687f2ce13430d6b0540a7026
siriusdemon <mory2016@126.com>
2019-05-22 12:37:33 +0800

    slice pass

--- books/TRPL/cp4_ownership/slice.rs
+++ books/TRPL/cp4_ownership/slice.rs
@@ -1,26 +1,44 @@
-// slice is a view of the real data
-// slice can be use as parameter
-// slice .......
-
-
+// Another data type that does not have ownership is the slice
+// A string slice is a reference to part of a String
+// For the purposes of introducing string slices, we are assuming ASCII only in this section
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-    
-    let slice = &a[1..3];
+    str_slice();    
+    int_slice();
 
-    println!("the slice of a is {:?}", slice);
-    
-
-    let s = "haha";
-    let len = length(s);
-    
-    println!("the length of s in {}", len);
-    println!("Yes, {} is still accessible", s);
+    let string = String::from("Mory and Ann");
+    let word = first_word(&string[..]);
+    println!("First word of string '{}' is {}", string, word);
 }
 
 
 // slice as parameter
+fn str_slice() {
+    let s = "haha"; // type of s is &str
+    let len = length(s);
+    
+    println!("the length of s in {}", len);
+    println!("Yes, {} is still accessible", s);
 
-fn length(s: &str) -> usize {
-    return s.len();
+    fn length(s: &str) -> usize {
+        return s.len();
+    }
+}
+
+
+fn int_slice() {
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    println!("the slice of int array is {:?}", slice);
+}
+
+
+// this signature accept string slice and String type
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
