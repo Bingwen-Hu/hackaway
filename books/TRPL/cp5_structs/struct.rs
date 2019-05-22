17b4558b6c11efdd8eeb141d3abfe7169e56ce16
siriusdemon <mory2016@126.com>
2019-05-22 21:22:41 +0800

    struct basic

--- books/TRPL/cp5_structs/struct.rs
+++ books/TRPL/cp5_structs/struct.rs
@@ -8,6 +8,7 @@ struct Rectangle{
 
 fn main() {
 
+    basic();
     let width = 39;
     let length = 12;
     let rect1 = Rectangle{width: 59, length: 29};
@@ -20,3 +21,34 @@ fn main() {
 // 1. [derive(Debug)] is needed
 // 2. when the var name and field name is same, a shortcut can be used.
 // 3. {:#?} is a pprint.
+
+fn basic() {
+    #[derive(Debug)]
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64, 
+        active: bool,
+    }
+    
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email, // shortcut
+            username,
+            active: true,
+            sign_in_count: 1,
+        }
+    }
+
+    // the whole variable should be mutable 
+    let mut user1 = build_user("Mory@bodhicitta".to_string(), "sakyamory".to_string());
+    let user2 = User {
+        email: String::from("Memory@bodhicitta"),
+        username: String::from("Jenny"),
+        ..user1
+    };
+
+    user1.email = String::from("Jenny2021@bodhicitta");
+    println!("user1 info {:#?}", user1);
+    println!("user1 info {:#?}", user2);
+}
