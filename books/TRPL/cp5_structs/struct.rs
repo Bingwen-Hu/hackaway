9f3c49aaa1aabd2ab294dab8c32ab612055123c3
siriusdemon <mory2016@126.com>
2019-05-23 10:38:54 +0800

    unitlike struct

--- books/TRPL/cp5_structs/struct.rs
+++ books/TRPL/cp5_structs/struct.rs
@@ -3,9 +3,13 @@
 // 2. when the var name and field name is same, a shortcut can be used.
 // 3. {:#?} is a pprint.
 
+// Unit-like structs can be useful in situations in which you need to 
+// implement a trait on some type but don’t have any data that you want to store in the type itself. 
+
 fn main() {
     basic();
     struct_tuple();
+    unit_struct();
 }
 
 
@@ -44,4 +48,11 @@ fn struct_tuple() {
     struct Color(u8, u8, u8);
     let c = Color(255, 10, 20);
     println!("Color is defined as struct tuple; c.1 is {}", c.1);
+}
+
+fn unit_struct() {
+    #[derive(Debug)]
+    struct UnitLike();
+    let u = UnitLike();
+    println!("I am a unit-like struct {:?}", u);
 }
