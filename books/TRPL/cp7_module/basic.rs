38cbc7eb359ab93f4edf145ab90786185490ce9a
siriusdemon <mory2016@126.com>
2019-05-27 14:52:21 +0800

    basic module

---
+++ books/TRPL/cp7_module/basic.rs
@@ -0,0 +1,18 @@
+// in src/main.rs of a project
+mod sound {
+    mod instrument {
+        mod woodwind {
+            fn clarinet() {}
+        }
+    }
+    mod voice {
+
+    }
+}
+
+fn main() {
+
+}
+
+// rust can define module using code only
+// which is similar to java but not python
