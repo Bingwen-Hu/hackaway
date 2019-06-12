671fc87b318d577c5de3b4844264d1757d6d3c38
siriusdemon <mory2016@126.com>
2019-06-12 18:56:27 +0800

    more usage ill

--- books/TRPL/cp7_module/basic.rs
+++ books/TRPL/cp7_module/basic.rs
@@ -1,17 +1,25 @@
 // in src/main.rs of a project
 mod sound {
-    mod instrument {
-        mod woodwind {
-            fn clarinet() {}
+    pub mod instrument {
+        pub mod woodwind {
+            pub fn clarinet() {
+                println!("sound::instrument::woodwind::clarinet() called!");
+            }
         }
     }
-    mod voice {
-
+    pub mod voice {
+        pub fn human() {
+            println!("Relative called!");
+            super::instrument::woodwind::clarinet();
+        }
     }
 }
 
 fn main() {
-
+    // relative called
+    sound::instrument::woodwind::clarinet();
+    // absolute called
+    crate::sound::voice::human();
 }
 
 // rust can define module using code only
