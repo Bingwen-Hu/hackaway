29231297ce8a4a41777bfbdfc10fb2405263d998
siriusdemon <mory2016@126.com>
2019-06-17 12:06:48 +0800

    facessh

---
+++ projects/facessh/facessh/api.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+import cv2
+import numpy as np
+import torch
+import torch.optim as optim
+
+from .model.dataset.factory import get_imdb
+from .model.utils.config import cfg
+from .model.roi_data_layer.layer import RoIDataLayer
+from .model.SSH import SSH
+from .model.network import save_check_point, load_check_point
+from .model.nms.nms_wrapper import nms
+from .model.utils.test_utils import _get_image_blob, _compute_scaling_factor, visusalize_detections
+
+
+
+thresh = 0.6
+gpu = None
+cwd = os.path.dirname(__file__)
+saved_model_path = os.path.join(cwd, 'check_point/check_point.zip')
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device(device)
+
+def load_model():
+    global device
+    net = SSH(vgg16_image_net=False)
+    check_point = load_check_point(saved_model_path)
+    net.load_state_dict(check_point['model_state_dict'])
+    net.to(device)
+    net.eval()
+    return net
+
+net = load_model()
+
+
+def detect(filepath):
+    global device
+    with torch.no_grad():
+        im = cv2.imread(filepath)
+        im_scale = _compute_scaling_factor(im.shape, cfg.TEST.SCALES[0], cfg.TEST.MAX_SIZE)
+        im_blob = _get_image_blob(im, [im_scale])[0]
+        im_info = np.array([[im_blob['data'].shape[2], im_blob['data'].shape[3], im_scale]])
+        im_data = im_blob['data']
+        im_info = torch.from_numpy(im_info).to(device)
+        im_data = torch.from_numpy(im_data).to(device)
+        batch_size = im_data.size()[0]
+        ssh_rois = net(im_data, im_info)
+        inds = (ssh_rois[:, :, 4] > thresh)
+        ssh_roi_keep = ssh_rois[:, inds[0], :]
+        ssh_roi_keep[:, :, 0:4] /= im_scale
+
+        for i in range(batch_size):
+            ssh_roi_single = ssh_roi_keep[i].cpu().numpy()
+            nms_keep = nms(ssh_roi_single, cfg.TEST.RPN_NMS_THRESH, force_cpu=True)
+            cls_dets_single = ssh_roi_single[nms_keep, :]
+    return cls_dets_single
+
+
