b7f40e5a50190b3a2d86c528821f99b7e5d3910c
siriusdemon <mory2016@126.com>
2019-06-18 18:45:20 +0800

    facerecommend first version

--- projects/facerec/facerec/api.py
+++ projects/facerec/facerec/api.py
@@ -16,12 +16,12 @@ def detect(im, mode='faster'):
 def _analyse(im, bbox):
     h, w = im.shape[:2]
     x1, y1, x2, y2, _ = bbox
-    margin = int((x2 - x1) * 0.4)
-    x1 = min(0, int(x1-margin))       
-    y1 = min(0, int(y1-margin))       
-    x2 = max(w, int(x2+margin))       
-    y2 = max(h, int(y2+margin))       
-    crop = im[y1:y2, x1:x2]
+    margin = int((x2 - x1) * 0.5)
+    x1_ = max(0, int(x1-margin))       
+    y1_ = max(0, int(y1-margin))       
+    x2_ = min(w, int(x2+margin))       
+    y2_ = min(h, int(y2+margin))       
+    crop = im[y1_:y2_, x1_:x2_]
     age, woman, _ = dex.estimate(crop)
     emotion_, _ = emotion.detect_cropped(crop)
     gender = 'woman' if woman > 0.5 else 'man'
@@ -29,8 +29,8 @@ def _analyse(im, bbox):
         'emotion': emotion_,
         'gender': gender,
         'age': int(age),
+        'bbox': [x1, y1, x2, y2],
     }
-
 
 
 if __name__ == "__main__":
