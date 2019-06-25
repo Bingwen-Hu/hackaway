c41ec53b50fbd7607f6f4a5fb88cd7a88fb0703e
siriusdemon <mory2016@126.com>
2019-06-25 20:27:54 +0800

    add pcn

--- projects/facerec/facerec/api.py
+++ projects/facerec/facerec/api.py
@@ -2,7 +2,7 @@ import emotion
 import dex
 import facessh
 import cv2
-
+import pcn
 
 
 def detect(im, mode='faster'):
@@ -16,14 +16,19 @@ def detect(im, mode='faster'):
 def _analyse(im, bbox):
     h, w = im.shape[:2]
     x1, y1, x2, y2, _ = bbox
-    margin = int((x2 - x1) * 0.5)
+    margin = int((x2 - x1)*0.5)
     x1_ = max(0, int(x1-margin))       
     y1_ = max(0, int(y1-margin))       
     x2_ = min(w, int(x2+margin))       
     y2_ = min(h, int(y2+margin))       
     crop = im[y1_:y2_, x1_:x2_]
-    age, woman, _ = dex.estimate(crop)
-    emotion_, _ = emotion.detect_cropped(crop)
+    winlist = pcn.detect(crop)
+    if len(winlist) != 0:
+        crop_pcn = pcn.crop(crop, winlist)[0]
+    else:
+        crop_pcn = crop
+    age, woman, _ = dex.estimate(crop_pcn)
+    emotion_, _ = emotion.detect_cropped(crop_pcn)
     gender = 'woman' if woman > 0.5 else 'man'
     return {
         'emotion': emotion_,
