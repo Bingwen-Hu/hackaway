1508c81c463d73b9655ad43c4aaacccb4f785806
siriusdemon <mory2016@126.com>
2019-06-29 20:22:47 +0800

    see env var

--- memos/rust/argv.rs
+++ memos/rust/argv.rs
@@ -6,4 +6,6 @@ fn main() {
     // collect is a function turn iterator into collections
     let args: Vec<String> = args.collect();
     println!("type of argv {:?}", args);
+    let cuda = env::var("CUDA_VISIBLE_DEVICES").unwrap();
+    println!("CUDA I use is {}", cuda);
 }
