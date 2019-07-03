d11ee17310a75dab5eafad709658c72e0926add0
siriusdemon <mory2016@126.com>
2019-07-03 12:29:44 +0800

    still bug

--- books/TRPL/cp13_functional/closure2.rs
+++ books/TRPL/cp13_functional/closure2.rs
@@ -4,6 +4,7 @@
 // new example in TRPL
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 
 
 // using closure instead of fn
@@ -12,6 +13,35 @@ use std::time::Duration;
 //     thread::sleep(Duration::from_secs(2));
 //     intensity
 // }
+
+struct Cacher<T>
+    where T: Fn(u32) -> u32 
+{
+    calculation: T, // a lambda
+    value_cache: HashMap<u32, u32>, // store the value of lambda execute
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value_cache: HashMap::new(),
+        }
+    }
+    
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value_cache.get(&arg) {
+            Some(v) => *v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value_cache.insert(arg, v);
+                v
+            },
+        }
+    }
+}
 
 fn generate_workout(intensity: u32, random_number: u32) {
     let expensive_closure = |num| {
