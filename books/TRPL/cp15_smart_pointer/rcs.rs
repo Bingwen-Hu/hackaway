e5f9fd8f9f3f7604fc0932d9059fd5b4b9cae451
siriusdemon <mory2016@126.com>
2019-07-05 11:30:25 +0800

    go to refcell

--- books/TRPL/cp15_smart_pointer/rcs.rs
+++ books/TRPL/cp15_smart_pointer/rcs.rs
@@ -12,10 +12,13 @@ use std::rc::Rc;
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
     println!("rc = {}", Rc::strong_count(&a));
-    let b = Cons(4, a.clone());
+    // Rc::clone is preferred for its clear meaning
+    // when use a.clone, we don't know whether it is a deep copy 
+    // or just increase count of references.
+    let b = Cons(4, Rc::clone(&a)); 
     println!("after add b to list, rc = {}", Rc::strong_count(&a));
     {
-        let c = Cons(6, a.clone());
+        let c = Cons(6, Rc::clone(&a));
         println!("after add c to list, rc = {}", Rc::strong_count(&a));
     }
     println!("c out of scope, rc = {}", Rc::strong_count(&a));
--- books/TRPL/cp15_smart_pointer/refcells.rs
+++ books/TRPL/cp15_smart_pointer/refcells.rs
@@ -2,6 +2,12 @@
 // interior mutability means mutating data even though there are 
 // immutable references to that data.
 
+// the RefCell<T> type represents single ownership over the data it holds. 
+// So, what makes RefCell<T> different from a type like Box<T>?
+// With references and Box<T>, the borrowing rules’ invariants are enforced 
+// at compile time. With RefCell<T>, these invariants are enforced at runtime. 
+// With references, if you break these rules, you’ll get a compiler error. 
+// With RefCell<T>, if you break these rules, your program will panic and exit.
 
 // something strange will happen!
 
