982f66051ef7b4dd48b5ba2726120e1f866618af
siriusdemon <mory2016@126.com>
2019-07-09 12:20:46 +0800

    longestsubstr

--- projects/interview/leetcode/leetcode/src/lib.rs
+++ projects/interview/leetcode/leetcode/src/lib.rs
@@ -1,4 +1,5 @@
 mod twosum;
+mod longestsubstr;
 
 #[cfg(test)]
 mod tests {
@@ -12,5 +13,17 @@ mod tests {
         let res = crate::twosum::two_sum_hash([3, 2, 4].to_vec(), 6);
         assert_eq!([1, 2].to_vec(), res);
     }
+
+    #[test]
+    fn test_longest_substr_a() {
+        let res = crate::longestsubstr::length_of_longest_substring("abcdefd".to_string());
+        assert_eq!(6, res);
+    }
+    #[test]
+    fn test_longest_substr_b() {
+        let res = crate::longestsubstr::length_of_longest_substring("abba".to_string());
+        assert_eq!(2, res);
+    }
 }
+
 
---
+++ projects/interview/leetcode/leetcode/src/longestsubstr.rs
@@ -0,0 +1,23 @@
+// tips: we only need the length
+// if we need the index, we need another two number 
+// to store the index
+use std::collections::HashMap;
+use std::cmp;
+
+pub fn length_of_longest_substring(s: String) -> i32 {
+    let mut map: HashMap<char, u32> = HashMap::new();
+    let mut start: u32 = 0;
+    let mut ans: u32 = 0;
+
+    for (end, c) in s.chars().enumerate() {
+        if let Some(&i) = map.get(&c) {
+            start = cmp::max(i, start);
+        }
+        ans = cmp::max(ans, (end as u32 - start + 1) as u32);
+        map.insert(c, end as u32 + 1);
+        println!("start {} end {} ans {} char {} map {:?}", 
+            start, end, ans, c, map 
+        );
+    }        
+    return ans as i32;
+}
