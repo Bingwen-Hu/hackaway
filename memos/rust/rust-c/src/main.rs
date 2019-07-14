6c6ae902f539c8ce419d56650e8d2180ce86d669
siriusdemon <mory2016@126.com>
2019-07-14 12:06:24 +0800

    it works

--- memos/rust/rust-c/src/main.rs
+++ memos/rust/rust-c/src/main.rs
@@ -1,11 +1,13 @@
 // very simple example call function from C library
 extern {
     pub fn puts(s: *const u8) -> i32;
+    fn hello();
 }
 
 fn main() {
     let x = b"hello world!\0";
     unsafe {
         puts(x.as_ptr());
+        hello();
     }
 }
