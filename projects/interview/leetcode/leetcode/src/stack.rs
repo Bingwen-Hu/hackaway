36fe657f86b408fcb34ac3121bb74e1b6ceddc9f
siriusdemon <mory2016@126.com>
2019-07-22 19:29:44 +0800

    improve version of stack

--- projects/interview/leetcode/leetcode/src/stack.rs
+++ projects/interview/leetcode/leetcode/src/stack.rs
@@ -8,19 +8,10 @@ pub fn is_valid(s: String) -> bool {
     map.insert('}', '{');
     let mut stack: Vec<char> = Vec::new();
     for c in s.chars() {
-        let pair = map.get(&c);
-        match pair {
-            Some(&p) => {
-                if stack.len() == 0 {
-                    return false;
-                } else {
-                    match stack.pop() {
-                        Some(x) => {
-                            if x != p { return false; }
-                        },
-                        _ => {},
-                    }
-                }
+        match map.get(&c) {
+            Some(&p) => match stack.pop() {
+                Some(x) => { if x != p { return false; } },
+                _ => return false,
             },
             _ => stack.push(c),
         }
