ced8c5194562c96b8ae18e363535bfb199d412f1
siriusdemon <mory2016@126.com>
2019-07-23 18:24:27 +0800

    fix reader bug

--- projects/deepfree/deepfree/datasets/coco/reader.py
+++ projects/deepfree/deepfree/datasets/coco/reader.py
@@ -9,19 +9,19 @@ class COCO(object):
     def __init__(self, images_directory, annotation_file):
         self.coco = coco.COCO(annotation_file)
         self.im_dir = images_directory
-        self.im_ids = np.array((self.coco.imgs.keys()))
+        self.im_ids = np.array(list(self.coco.imgs.keys()))
         self.size = len(self.im_ids)
     
     def fetch(self, batch_size, random=False):
         if random:
             self.im_ids = np.random.shuffle(self.im_ids)
         
-        iters = self.size / batch_size + 1 # number of iteration
+        iters = self.size // batch_size + 1 # number of iteration
         for i in range(iters):
             start, end = i*batch_size, min((i+1)*batch_size, self.size)
             im_ids = self.im_ids[start:end]
             ann_ids = [self.coco.getAnnIds(im_id) for im_id in im_ids]
-            im_metas = [self.coco.loadImgs(im_id) for im_id in im_ids]
+            im_metas = self.coco.loadImgs(im_ids)
             ann_metas = [self.coco.loadAnns(ann_id) for ann_id in ann_ids]
             yield (im_metas, ann_metas)
 
--- projects/deepfree/tests/test_dataset.py
+++ projects/deepfree/tests/test_dataset.py
@@ -1,6 +1,15 @@
+import os.path as osp
+
 from deepfree.datasets.coco import reader
 
 
-images_directory = '/media/data/'
+COCO_DIR = "/media/data/urun_tandong_video/data/COCO/"
+images_directory = osp.join(COCO_DIR, 'images/train2014/')
+annotations_file = osp.join(COCO_DIR, "annotations/person_keypoints_train2014.json")
 
-coco = reader.COCO()
+
+coco = reader.COCO(images_directory, annotations_file)
+data_generator = coco.fetch(batch_size=10)
+im_metas, ann_metas = next(data_generator)
+assert len(im_metas) == 10, "batch_size not match!"
+assert im_metas[0] is not None, "return None!"
