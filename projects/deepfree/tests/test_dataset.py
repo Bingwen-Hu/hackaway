17b490fea6f03b792f55fd053fac6be213af5cdf
siriusdemon <mory2016@126.com>
2019-07-23 18:59:01 +0800

    use unit test instead

--- projects/deepfree/tests/test_dataset.py
+++ projects/deepfree/tests/test_dataset.py
@@ -1,20 +1,26 @@
 import sys
+import os
 import os.path as osp
-
+# add into PYTHONPATH
 sys.path.insert(0, os.path.join(os.path.abspath('..'), 'source_test'))
-
 
 
 from deepfree.datasets.coco import reader
 
-
-COCO_DIR = "/media/data/urun_tandong_video/data/COCO/"
-images_directory = osp.join(COCO_DIR, 'images/train2014/')
-annotations_file = osp.join(COCO_DIR, "annotations/person_keypoints_train2014.json")
+import unittest
 
 
-coco = reader.COCO(images_directory, annotations_file)
-data_generator = coco.fetch(batch_size=10)
-im_metas, ann_metas = next(data_generator)
-assert len(im_metas) == 10, "batch_size not match!"
-assert im_metas[0] is not None, "return None!"
+class TestDatasets(unittest.TestCase):
+    def setUp(self):
+        COCO_DIR = "/media/data/urun_tandong_video/data/COCO/"
+        images_directory = osp.join(COCO_DIR, 'images/train2014/')
+        annotations_file = osp.join(COCO_DIR, "annotations/person_keypoints_train2014.json")
+        self.reader = reader.COCO(images_directory, annotations_file)
+    
+    def test_fetch(self):
+        data_generator = self.reader.fetch(batch_size=10)
+        im_metas, ann_metas = next(data_generator)
+        self.assertEquals(len(im_metas), 10)
+        self.assertEquals(len(ann_metas), 10)
+        self.assertIsNotNone(im_metas[0])
+        
