83acb27fac87330310d8a0e061add7c797ea2fbf
siriusdemon <mory2016@126.com>
2019-08-08 20:34:54 +0800

    update gradient

--- projects/interview/others/gradient.py
+++ projects/interview/others/gradient.py
@@ -1,22 +1,23 @@
+# Buggy code
 # Gradient for y = w1
 
 import numpy as np
 
-def gradient_descent(x, y, lr=0.001, threshold=1e-6):
+def gradient_descent(x, y, lr=0.01, threshold=1e-3):
     params = np.array([0.1, 0.2, 0])
     x = np.array([x[0], x[1], 1])
     y_h = np.sum(x * params)
     loss = 1 / 2 * np.square(y - y_h)
     while loss > threshold:
-        params = params + lr * x
+        params = params - lr * x * -(y - y_h)
         y_h = np.sum(x * params)
         loss = 1 / 2 * np.square(y - y_h)
         print(loss)
 
     return params, loss
 
-x = [1.2, 0.4]
-y = 5
+x = [-1, 0.4]
+y = 20
 params, loss = gradient_descent(x, y)
 
 # validate 
