7150eadc933dce7868eded3e610681dadc77ea2d
siriusdemon <mory2016@126.com>
2019-08-15 21:01:22 +0800

    base

--- projects/interview/others/base.py
+++ projects/interview/others/base.py
@@ -1,9 +1,13 @@
 import sys
 
-lines = sys.stdin.readlines()
-n, m = list(map(int, lines[0].split(' ')))
-num1 = list(map(int, lines[1].split(' ')))
-num2 = list(map(int, lines[2].split(' ')))
+# lines = sys.stdin.readlines()
+# n, m = list(map(int, lines[0].split(' ')))
+# num1 = list(map(int, lines[1].split(' ')))
+# num2 = list(map(int, lines[2].split(' ')))
+
+n, m = 5, 5
+num1 = [4, 4, 1, 1, 1]
+num2 = [4, 3, 0, 1, 2]
 
 # 返回值
 ret = []
@@ -19,8 +23,8 @@ while len(num1) > 0:
                 xj = n2
 
     ret.append(maximum)
-    num1.remove(n1)
-    num2.remove(n2)
+    num1.remove(xi)
+    num2.remove(xj)
     maximum = -1
 
 
