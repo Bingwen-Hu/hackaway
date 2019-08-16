1c62a87f71247d4213416eb1d59430c6d90e3668
siriusdemon <mory2016@126.com>
2019-08-16 19:18:04 +0800

    cut

---
+++ projects/interview/others/cut.py
@@ -0,0 +1,27 @@
+# 输入木棍，切成几段，使其乘积最大
+
+import sys
+n = sys.stdin.readlines()
+n = int(n.strip())
+
+def cut(n):
+    best = [0,1,2,3]
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:  # m > 1
+        return 2
+    for j in range(4, n+1):
+        maximum = 0
+        for i in range(1,j):
+            temp = best[i] * best[j-i]
+            if temp > maximum:
+                maximum = temp
+        best.append(maximum) 
+    return best[-1]
+
+ans = cut(n)
+print(ans)
