5517a0b815442acb56c8c0bae2fc1efa3b225cba
siriusdemon <mory2016@126.com>
2019-10-06 14:50:41 +0800

    add sparse into system

--- projects/modern/C/test/sparse.h
+++ projects/modern/C/test/sparse.h
@@ -1,9 +1,1 @@
-// CCS representation
-#pragma once
-
-
-void sparse_pack(double** a, int m, int n,
-        int* Ap, int* Ai, double* Ax);
-
-void sparse_unpack(double** a, int m, int n, 
-        int* Ap, int* Ai, double* Ax);
+../src/sparse.h
