9a17f9429d21620d3fcc44a1179f299494abb9da
siriusdemon <mory2016@126.com>
2019-10-08 10:19:47 +0800

    umfpack as system pack

---
+++ projects/modern/C/test/test-umfpack.c
@@ -0,0 +1,57 @@
+#include <suitesparse/umfpack.h>
+#include <stdio.h>
+#include "array.h"
+#include "sparse.h"
+
+int main()
+{
+    // init a matrix
+    double** a;
+    double* b;
+    double* x; // for solved result
+    int n = 5;
+    make_matrix(a, n, n); 
+    make_vector(b, n);
+    make_vector(x, n);
+
+    a[0][0] = 2; a[0][1] = 3; a[0][2] = 0; a[0][3] = 0; a[0][4] = 0;
+    a[1][0] = 3; a[1][1] = 0; a[1][2] = 4; a[1][3] = 0; a[1][4] = 6;
+    a[2][0] = 0; a[2][1] = -1; a[2][2] = -3; a[2][3] = 2; a[2][4] = 0;
+    a[3][0] = 0; a[3][1] = 0; a[3][2] = 1; a[3][3] = 0; a[3][4] = 0;
+    a[4][0] = 0; a[4][1] = 4; a[4][2] = 2; a[4][3] = 0; a[4][4] = 1;
+    
+    b[0] = 8; b[1] = 45; b[2] = -3; b[3] = 3; b[4] = 19;
+
+    // solution: x = 1 2 3 4 5
+
+    int nonzero = 12;
+
+    int* Ap;
+    int* Ai;
+    double* Ax;
+    make_vector(Ap, n+1);
+    make_vector(Ai, nonzero);
+    make_vector(Ax, nonzero);
+
+    sparse_pack(a, n, n, Ap, Ai, Ax);
+
+    void* symbolic;
+    void* numeric;
+    umfpack_di_symbolic(n, n, Ap, Ai, Ax, &symbolic, NULL, NULL);
+    umfpack_di_numeric(Ap, Ai, Ax, symbolic, &numeric, NULL, NULL);
+    umfpack_di_solve(UMFPACK_A, Ap, Ai, Ax, x, b, numeric, NULL, NULL);
+
+    print_vector("%3.1lf ", x, n);
+
+    umfpack_di_free_symbolic(&symbolic);
+    umfpack_di_free_numeric(&numeric);
+
+    free_matrix(a);
+    free_vector(b);
+    free_vector(x);
+    free_vector(Ap);
+    free_vector(Ai);
+    free_vector(Ax);
+
+    return 0;
+}
