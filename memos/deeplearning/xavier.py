46306fd54f05d3b3d996cc4ec66490d2f36c61c9
siriusdemon <mory2016@126.com>
2019-10-09 11:48:45 +0800

    add xavier

---
+++ memos/deeplearning/xavier.py
@@ -0,0 +1,11 @@
+import math
+
+input_size = (3, 100, 100)
+
+def xavier_init(size):
+    """
+    input dimension is `dim`
+    F = 1 / sqrt(dim / 2) 
+    """
+    dim = size[0]
+    xavier_stddev = 1. / math.sqrt(in_dim / 2.)
