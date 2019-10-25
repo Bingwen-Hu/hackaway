1bc2d0a6c277c2cc7c0266c952d6af7ff80a723f
siriusdemon <mory2016@126.com>
2019-10-25 13:31:45 +0800

    leetcode.py

---
+++ projects/interview/leetcode/leetcode.py
@@ -0,0 +1,13 @@
+# https://leetcode-cn.com/problems/container-with-most-water/
+def maxArea(heights) -> int:
+    """盛水最多的容器"""
+    return 0
+
+def test_maxArea():
+    val = maxArea([1,8,6,2,5,4,8,3,7])
+    expect = 49
+    assert val == expect
+
+
+
+
